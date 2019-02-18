7eaf5c1c43b54c6487fba1a1c695141f6601ccac
GS Tearm <gstearmit@gmail.com>
2019-02-18 13:46:21 +0700

    weshopdb

--- weshop_global.sql
+++ weshop_global.sql
@@ -4,13 +4,60 @@
 CREATE TABLE `Customer`
 (
  `customer_id`   int NOT NULL AUTO_INCREMENT ,
- `customer_name` varchar(40) NOT NULL ,
- `customer_phone`        varchar(20) ,
+ `organisation_or_person` varchar(40) NOT NULL COMMENT 'Tên tổ chức hoặc tên người ' ,
+ `organisation_or_name` varchar(40) NOT NULL COMMENT ' Tên tổ chức  ' ,
+ `gender` varchar(40) NOT NULL COMMENT '   ' ,
+ `first_name` varchar(40) NOT NULL COMMENT '   ' ,
+ `middle_initial` varchar(40) NOT NULL COMMENT ' Tên đệm ' ,
+ `last_name` varchar(40) NOT NULL COMMENT '' ,
+ `email_address` varchar(40) NOT NULL COMMENT '' ,
+ `login_name` varchar(40) NOT NULL COMMENT '' ,
+ `login_password` varchar(40) NOT NULL COMMENT '' ,
+ `phone_number`  varchar(20) ,
+ `address_line_1` varchar(255) NOT NULL COMMENT '' ,
+ `address_line_2` varchar(255) NOT NULL COMMENT '' ,
+ `address_line_3` varchar(255) NOT NULL COMMENT '' ,
+ `address_line_4` varchar(255) NOT NULL COMMENT '' ,
+ `town_city` varchar(40) NOT NULL COMMENT '' ,
+ `country` varchar(40) NOT NULL COMMENT '' ,
  `inserted_at`  datetime NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT 'timestamp with time zone = not null',
  `updated_at`  datetime NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT 'timestamp with time zone = not null',
 PRIMARY KEY (`customer_id`),
-UNIQUE KEY `AK1_Customer_customer_name` (`customer_name`)
+UNIQUE KEY `AK1_Customer_email_address` (`email_address`)
 ) AUTO_INCREMENT=1 COMMENT='Basic information about Customer';
+
+
+-- ************************************** `CustomerPaymentMethods`
+
+CREATE TABLE `CustomerPaymentMethods`
+(
+  `customer_payment_id`   int NOT NULL AUTO_INCREMENT ,
+  `customer_id` int NOT NULL ,
+  `payment__method_id` varchar(200) NOT NULL COMMENT '' ,
+  `credit_card_number` varchar(255) NOT NULL COMMENT '' ,
+  `payment_method_details` varchar(255) NOT NULL COMMENT '' ,
+  `inserted_at`  datetime NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT 'timestamp with time zone = not null',
+  `updated_at`  datetime NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT 'timestamp with time zone = not null',
+  PRIMARY KEY (`customer_id`),
+  KEY `FK_CustomerPaymentMethods_customer_id_Customer` (`customer_id`),
+  CONSTRAINT `FK_CustomerPaymentMethods_customer_id_Customer` FOREIGN KEY `FK_CustomerPaymentMethods_customer_id_Customer` (`customer_id`) REFERENCES `Customer` (`customer_id`)
+) AUTO_INCREMENT=1 COMMENT='Basic information about CustomerPaymentMethods';
+
+
+-- ************************************** `RefPaymentMethods`
+CREATE TABLE `RefPaymentMethods`
+(
+  `payment__method_id`   int NOT NULL AUTO_INCREMENT ,
+  `customer_id` int NOT NULL ,
+  `payment_method_code` varchar(200) NOT NULL COMMENT '' ,
+  `payment_method_description` varchar(255) NOT NULL COMMENT ' -eg CC=Credit Card' ,
+  `inserted_at`  datetime NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT 'timestamp with time zone = not null',
+  `updated_at`  datetime NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT 'timestamp with time zone = not null',
+  PRIMARY KEY (`payment__method_id`),
+  KEY `FK_CustomerPaymentMethods_customer_id_Customer` (`customer_id`),
+  CONSTRAINT `FK_CustomerPaymentMethods_customer_id_Customer` FOREIGN KEY `FK_CustomerPaymentMethods_customer_id_Customer` (`customer_id`) REFERENCES `Customer` (`customer_id`)
+) AUTO_INCREMENT=1 COMMENT='Basic information about RefPaymentMethods cần làm rõ hơn nữa';
+
 
 
 -- ************************************** `Order`
@@ -21,6 +68,7 @@ CREATE TABLE `Order`
  `order_number` varchar(10) ,
  `customer_id`  int NOT NULL ,
  `order_status_id`  int NOT NULL ,
+ `coupons_id`  int NOT NULL ,
  `order_details` varchar(255) COMMENT 'mô tả đơn hàng' ,
  `order_date`   datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ,
  `total_amount` decimal(12,2) NOT NULL ,
@@ -31,9 +79,50 @@ UNIQUE KEY `AK1_Order_order_number` (`order_number`),
 KEY `FK_Order_customer_id_Customer` (`customer_id`),
 CONSTRAINT `FK_Order_customer_id_Customer` FOREIGN KEY `FK_Order_customer_id_Customer` (`customer_id`) REFERENCES `Customer` (`customer_id`),
 KEY `FK_Order_order_status_id_OrderStatus` (`order_status_id`),
-CONSTRAINT `FK_Order_order_status_id_OrderStatus` FOREIGN KEY `FK_Order_order_status_id_OrderStatus` (`order_status_id`) REFERENCES `OrderStatus` (`order_status_id`)
+CONSTRAINT `FK_Order_order_status_id_OrderStatus` FOREIGN KEY `FK_Order_order_status_id_OrderStatus` (`order_status_id`) REFERENCES `OrderStatus` (`order_status_id`),
+KEY `FK_Order_coupons_id_Coupons` (`coupons_id`),
+CONSTRAINT `FK_Order_coupons_id_Coupons` FOREIGN KEY `FK_Order_coupons_id_Coupons` (`coupons_id`) REFERENCES `Coupons` (`coupons_id`)
 ) AUTO_INCREMENT=1 COMMENT='Order information like Date, Ammount';
 
+
+-- ************************************** `Transactions`
+
+CREATE TABLE `Transactions`
+(
+  `transaction_id`     int NOT NULL AUTO_INCREMENT ,
+  `transaction_code` varchar(255) ,
+  `transaction_date`   datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ,
+  `order_id`  int NOT NULL ,
+  `processor` varchar(255) NOT NULL ,
+  `processor_trans_id` varchar(255) NOT NULL ,
+  `amount` NUMERIC NOT NULL ,
+  `cc_number` varchar(255) NOT NULL ,
+  `cc_type` varchar(255) NOT NULL ,
+  `response` TEXT,
+  `inserted_at`  datetime NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT 'timestamp with time zone = not null',
+  `updated_at`  datetime NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT 'timestamp with time zone = not null',
+  PRIMARY KEY (`order_id`),
+  KEY `FK_Transactions_order_id_Order` (`order_id`),
+  CONSTRAINT `FK_Transactions_order_id_Order` FOREIGN KEY `FK_Transactions_order_id_Order` (`order_id`) REFERENCES `Order` (`order_id`)
+) AUTO_INCREMENT=1 COMMENT='Transactions ';
+
+
+-- ************************************** `Coupons`
+
+CREATE TABLE `Coupons`
+(
+  `coupons_id`     int NOT NULL AUTO_INCREMENT ,
+  `coupons_code` varchar(255) ,
+  `description`   TEXT ,
+  `active`  BOOLEAN ,
+  `value` NUMERIC COMMENT 'default = 1 active',
+  `multiple` BOOLEAN COMMENT 'default = 0 False',
+  `start_date`   datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ,
+  `end_date`   datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ,
+  `inserted_at`  datetime NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT 'timestamp with time zone = not null',
+  `updated_at`  datetime NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT 'timestamp with time zone = not null',
+  PRIMARY KEY (`coupons_id`)
+) AUTO_INCREMENT=1 COMMENT='Coupons ';
 
 -- ************************************** `OrderStatus`
 
@@ -138,6 +227,7 @@ CREATE TABLE `OrderItem`
  `order_item_id`     int NOT NULL AUTO_INCREMENT ,
  `order_id`   int NOT NULL ,
  `product_id` int NOT NULL ,
+ `orderitem_status_id`   int NOT NULL ,
  `unit_price` decimal(12,2) NOT NULL ,
  `order_tem_quantity`  int NOT NULL ,
  `inserted_at`  datetime NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT 'timestamp with time zone = not null',
@@ -146,7 +236,9 @@ CREATE TABLE `OrderItem`
  KEY `FK_OrderItem_order_id_Order` (`order_id`),
  CONSTRAINT `FK_OrderItem_order_id_Order` FOREIGN KEY `FK_OrderItem_order_id_Order` (`order_id`) REFERENCES `Order` (`order_id`),
  KEY `FK_OrderItem_product_id_Product` (`product_id`),
- CONSTRAINT `FK_OrderItem_product_id_Product` FOREIGN KEY `FK_OrderItem_product_id_Product` (`product_id`) REFERENCES `Product` (`product_id`)
+ CONSTRAINT `FK_OrderItem_product_id_Product` FOREIGN KEY `FK_OrderItem_product_id_Product` (`product_id`) REFERENCES `Product` (`product_id`),
+ KEY `FK_OrderItem_orderitem_status_id_OrderItemStatus` (`orderitem_status_id`),
+ CONSTRAINT `FK_OrderItem_orderitem_status_id_OrderItemStatus` FOREIGN KEY `FK_OrderItem_orderitem_status_id_OrderItemStatus` (`orderitem_status_id`) REFERENCES `OrderItemStatus` (`orderitem_status_id`)
 ) AUTO_INCREMENT=1  COMMENT='Information about like Price, quantity';
 
 
