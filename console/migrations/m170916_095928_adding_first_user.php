9123259623a3905bb33ec1c0a898ab20f6973f0e
GS Tearm <gstearmit@gmail.com>
2019-02-19 15:19:02 +0700

    WS db 2019

--- console/migrations/m170916_095928_adding_first_user.php
+++ console/migrations/m170916_095928_adding_first_user.php
@@ -13,8 +13,8 @@ class m170916_095928_adding_first_user extends Migration
     public function safeUp()
     {
         $sql="INSERT INTO `user` (`id`, `username`, `auth_key`, `password_hash`, `password_reset_token`, `email`, `status`, `created_at`, `updated_at`) VALUES
-(1, 'sirinibin', 'msYYcnCvsik9p-xXFh917VQ48fyzhtvR', '$2y$13$4M3N06yd6/sjwcKcal8PceB1nt.x6fJnxEx73iKZZV.tF1KPeILlO', 'mP4czGphDbp3waFgWSRyAPw5uBzgxmiQ_1429844420', 'sirinibin2006@gmail.com', 10, 1429844102, 1429846978);
-";
+            (1, 'weshopgl', 'msYYcnCvsik9p-xXFh917VQ48fyzhtvR', '$2y$13$4M3N06yd6/sjwcKcal8PceB1nt.x6fJnxEx73iKZZV.tF1KPeILlO', 'mP4czGphDbp3waFgWSRyAPw5uBzgxmiQ_1429844420', 'weshopgl2019@gmail.com', 10, 1429844102, 1429846978);
+            ";
         Yii::$app->db->createCommand($sql)->execute();
     }
 
