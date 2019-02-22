c1b3e6d2035ff1fa32b7d1adf23a52741f193e76
VienLV <VienLV@example.com>
2019-02-22 17:19:48 +0700

    add data faker

---
+++ common/fixtures/data/store.php
@@ -0,0 +1,18 @@
+<?php
+
+return [
+    'store0' => [
+        'id' => 1,
+        'country_id' => 1,
+        'locale' => 'vi',
+        'name' => 'Việt Nam',
+        'country_name' => 'Việt Nam',
+        'address' => '656, Thôn Nhượng Bảo, Phường Siêu Trọng, Quận 51
+Hà Nam',
+        'url' => 'https://thinh.gov.vn/ut-modi-in-ut-molestias-et-ut.html',
+        'currency' => 'VNĐ',
+        'currency_id' => 1,
+        'status' => 1,
+        'env' => "ENV",
+    ]
+];
