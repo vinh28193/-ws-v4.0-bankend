da06081954835765f3d65ed76253d69739bc64ef
GS Tearm <gstearmit@gmail.com>
2019-02-23 13:39:08 +0700

    Back end Monitor API all

--- common/config/bootstrap.php
+++ common/config/bootstrap.php
@@ -1,5 +1,6 @@
 <?php
 Yii::setAlias('@common', dirname(__DIR__));
 Yii::setAlias('@api', dirname(dirname(__DIR__)) . '/api');
-Yii::setAlias('@apiops', dirname(dirname(__DIR__)) . '/apiops');
+Yii::setAlias('@backend', dirname(dirname(__DIR__)) . '/backend');
+Yii::setAlias('@userbackend', dirname(dirname(__DIR__)) . '/userbackend');
 Yii::setAlias('@console', dirname(dirname(__DIR__)) . '/console');
---
+++ userbackend/config/params.php
@@ -0,0 +1,7 @@
+<?php
+return [
+    'adminEmail' => 'phuchc@peacesoft.net',
+    'API_VERSION'=>'1',
+    'dev_api_url'=>'weshop-v4.back-end.local.vn',
+    'prod_api_url'=>'weshop-v4.back-end.local.vn'
+];
