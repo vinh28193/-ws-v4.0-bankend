b4d39ed6901597f3f2d6b287282eb3a0459dac21
VienLV <VienLV@example.com>
2019-02-23 14:17:53 +0700

    add auth frontend

---
+++ backend/modules/v1/weshop/customer/Module.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace backend\modules\v1\weshop\customer;
+
+/**
+ * customer module definition class
+ */
+class Module extends \yii\base\Module
+{
+    /**
+     * {@inheritdoc}
+     */
+    public $controllerNamespace = 'backend\modules\v1\weshop\customer\controllers';
+
+    /**
+     * {@inheritdoc}
+     */
+    public function init()
+    {
+        parent::init();
+
+        // custom initialization code goes here
+    }
+}
---
+++ console/migrations/m190223_065437_user_name_to_username.php
@@ -0,0 +1,46 @@
+<?php
+
+use yii\db\Migration;
+
+/**
+ * Class m190223_065437_user_name_to_username
+ */
+class m190223_065437_user_name_to_username extends Migration
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function safeUp()
+    {
+        $this->renameColumn('customer','user_name','username');
+        $this->renameColumn('customer','password','password_hash');
+        $sql = "ALTER TABLE `customer` 
+MODIFY COLUMN `remove` tinyint(4) NULL DEFAULT 0 AFTER `active`";
+        Yii::$app->db->createCommand($sql)->execute();
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function safeDown()
+    {
+        echo "m190223_065437_user_name_to_username cannot be reverted.\n";
+
+        return false;
+    }
+
+    /*
+    // Use up()/down() to run migration code without a transaction.
+    public function up()
+    {
+
+    }
+
+    public function down()
+    {
+        echo "m190223_065437_user_name_to_username cannot be reverted.\n";
+
+        return false;
+    }
+    */
+}
