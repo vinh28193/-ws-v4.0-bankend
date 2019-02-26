058630b2673df94acf3d45edc79e782ccdcd78a4
Vinh (envy) <vinhvv@peacesoft.net>
2019-02-26 10:27:44 +0700

    unit

--- common/tests/unit/components/AdditionalFeeTest.php
+++ common/tests/unit/components/AdditionalFeeTest.php
@@ -1,10 +1,13 @@
 <?php namespace common\tests\components;
 
+use Codeception\Test\Unit;
+use common\components\StoreManager;
+use common\tests\_support\ActiveStore;
 use common\tests\_support\AdditionalFeeObject;
 use common\tests\_support\TestCondition;
 use common\tests\_support\StoreAdditionalFee;
 
-class AdditionalFeeTest extends \Codeception\Test\Unit
+class AdditionalFeeTest extends Unit
 {
     /**
      * @var \common\tests\UnitTester
@@ -19,7 +22,14 @@ class AdditionalFeeTest extends \Codeception\Test\Unit
     {
     }
 
-    public function testGetStoreManager()
+    public function testGetStoreManager(){
+        $additionalFeeTest = new AdditionalFeeObject();
+        $class = $additionalFeeTest->getStoreManager();
+        $class = get_class($class);
+        $this->assertEquals($class, StoreManager::className());
+    }
+
+    public function testSetAdditionalFees()
     {
         $additionalFeeTest = new AdditionalFeeObject();
         $condition = new TestCondition();
@@ -27,14 +37,13 @@ class AdditionalFeeTest extends \Codeception\Test\Unit
         $storeAdditionalFee->name = 'test_fee';
         $storeAdditionalFee->condition_name = $condition->name;
         $storeAdditionalFee->condition_data = serialize($condition);
+        $storeAdditionalFee->is_convert = 1;
         $additionalFeeTest->storeAdditionalFee['test_fee'] = $storeAdditionalFee;
         $fee = ['test_fee' => 999];
         $withCondition = true;
         $ensureReadOnly = false;
 
         $additionalFeeTest->setAdditionalFees($fee, $withCondition, $ensureReadOnly);
-        $this->assertEquals($additionalFeeTest->total_test_fee_local, 999);
-        $additionalFeeTest->storeAdditionalFee['test_fee']->is_convert = 1;
         $this->assertEquals($additionalFeeTest->total_test_fee_local, 999 * $additionalFeeTest->getExchangeRate());
     }
 }
