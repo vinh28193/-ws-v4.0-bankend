08dd27a795dc4f41da40f760b736a620e912c74c
VienLV <VienLV@example.com>
2019-02-26 14:14:43 +0700

    add fake package

---
+++ common/fixtures/PackageFixture.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: galat
+ * Date: 26/02/2019
+ * Time: 14:03
+ */
+
+namespace common\fixtures;
+
+
+use yii\test\ActiveFixture;
+
+class PackageFixture extends ActiveFixture
+{
+    public $modelClass = 'common\models\db\Product';
+    public $depends = [
+        'common\fixtures\OrderFixture',
+    ];
+}
---
+++ common/fixtures/templates/package.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: galat
+ * Date: 26/02/2019
+ * Time: 14:03
+ */
+
+return [
+    'id' => 'ID',
+    'package_code' => 'Package Code',
+    'tracking_seller' => 'Tracking Seller',
+    'order_ids' => 'Order Ids',
+    'tracking_reference_1' => 'Tracking Reference 1',
+    'tracking_reference_2' => 'Tracking Reference 2',
+    'manifest_code' => 'Manifest Code',
+    'package_weight' => 'Package Weight',
+    'package_change_weight' => 'Package Change Weight',
+    'package_dimension_l' => 'Package Dimension L',
+    'package_dimension_w' => 'Package Dimension W',
+    'package_dimension_h' => 'Package Dimension H',
+    'seller_shipped' => 'Seller Shipped',
+    'stock_in_us' => 'Stock In Us',
+    'stock_out_us' => 'Stock Out Us',
+    'stock_in_local' => 'Stock In Local',
+    'lost' => 'Lost',
+    'current_status' => 'Current Status',
+    'warehouse_id' => 'Warehouse ID',
+    'created_at' => 'Created Time',
+    'updated_at' => 'Updated Time',
+];
