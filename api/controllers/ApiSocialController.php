cb14085f44083c09165400865f6b9390e70dd102
Hoang Cong Phuc <phuchc@peacesoft.net>
2019-03-05 16:46:01 +0700

    add api social

---
+++ api/controllers/ApiSocialController.php
@@ -0,0 +1,88 @@
+<?php
+
+namespace api\controllers;
+
+
+use yii\filters\AccessControl;
+use api\behaviours\Verbcheck;
+use api\behaviours\Apiauth;
+
+use Yii;
+
+
+
+class ApiSocialController extends RestController
+{
+
+    public function behaviors()
+    {
+
+        $behaviors = parent::behaviors();
+
+        return $behaviors + [
+
+                'apiauth' => [
+                    'class' => Apiauth::className(),
+                    'exclude' => ['convert-token'],
+                    'callback'=>[]
+                ],
+                'access' => [
+                    'class' => AccessControl::className(),
+                    'only' => ['index'],
+                    'rules' => [
+                        [
+                            'actions' => [],
+                            'allow' => true,
+                            'roles' => ['?'],
+                        ],
+                        [
+                            'actions' => [
+                                'index'
+                            ],
+                            'allow' => true,
+                            'roles' => ['@'],
+                        ],
+                        [
+                            'actions' => ['convert-token'],
+                            'allow' => true,
+                            'roles' => ['*'],
+                        ],
+                    ],
+                ],
+                'verbs' => [
+                    'class' => Verbcheck::className(),
+                    'actions' => [
+                        'index' => ['GET', 'POST'],
+                        'convert-token' => ['POST'],
+                    ],
+                ],
+
+            ];
+    }
+/**
+* @inheritdoc
+*/
+    public function actions()
+    {
+        return [
+            'error' => [
+                'class' => 'yii\web\ErrorAction',
+            ],
+            'auth' => [
+                'class' => 'yii\authclient\AuthAction',
+                'successCallback' => [$this, 'onAuthSuccess'],
+            ],
+        ];
+    }
+
+    public function actionIndex()
+    {
+        Yii::$app->api->sendSuccessResponse(['Yii2 RESTful ApiSocial API with OAuth2']);
+    }
+
+    public function actionConvertToken()
+    {
+        Yii::$app->api->sendSuccessResponse(['Yii2 RESTful convert-token POST ApiSocial API with OAuth2']);
+    }
+
+}
