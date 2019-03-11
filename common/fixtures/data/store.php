75a9a3350109ae903c0a6358dfdda2d013a2c794
Hoang Cong Phuc <phuchc@peacesoft.net>
2019-03-11 15:15:00 +0700

    update role + readme

--- common/fixtures/data/store.php
+++ common/fixtures/data/store.php
@@ -8,7 +8,7 @@ return [
         'name' => 'Việt Nam',
         'country_name' => 'Việt Nam',
         'address' => 'Tầng 16, tòa nhà VTC Online số 18 đường Tam Trinh, Phường Minh Khai, Quận Hai Bà Trưng, Thành phố Hà Nội, Việt Nam',
-        'url' => 'http://weshop-v4.front-end.local.vn/',
+        'url' => 'weshop-v4.back-end.local.vn',
         'app_id' => 'WESHOPVN',
         'store_alias' => 'WESHOPVN',
         'currency' => 'VNĐ',
