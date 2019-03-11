2678fc5ee7af46859e9d20399194039fc1065eeb
Hoang Cong Phuc <phuchc@peacesoft.net>
2019-03-11 09:43:16 +0700

    update automaintin test done

--- api/tests/api/PostsCest.php
+++ api/tests/api/PostsCest.php
@@ -10,56 +10,57 @@ class PostsApiCest
 {
     function _before(ApiTester $I)
     {
+        /*
         $I->haveFixtures([
             'post' => [
                 'class' => PostFixture::className(),
                 'dataFile' => codecept_data_dir() . 'post.php'
             ]
         ]);
+        */
     }
 
     public function testGetAll(ApiTester $I)
     {
         $I->haveHttpHeader('Content-Type','application/json');
-        $I->haveHttpHeader('X-Access-Token','c2c41e1d66e70e3b671182a0fc6eca56');
+        $I->haveHttpHeader('X-Access-Token','3fea2609cbbeb95b557c503c6e4e5e9c');
         $I->sendGET('/1/post');
         $I->seeResponseCodeIs(200);
         $I->seeResponseCodeIsSuccessful();
         $I->seeResponseIsJson();
-        //$I->seeResponseContains('{ "status": 1, "data": [ { "id": "1", "name": "John ","email": "john@gmail.com"');
+        $I->seeResponseContainsJson(["title" => "Weshop"]);
     }
 
     public function testGetOne(ApiTester $I)
     {
         $I->haveHttpHeader('Content-Type','application/json');
-        $I->haveHttpHeader('X-Access-Token','c2c41e1d66e70e3b671182a0fc6eca56');
-        $I->sendGET('/1/post/1');
-        $I->seeResponseCodeIs(404);
-        //$I->seeResponseCodeIsSuccessful();
-        //$I->seeResponseIsJson();
-        //$I->seeResponseContainsJson(['title' => 'First Post']);
+        $I->haveHttpHeader('X-Access-Token','3fea2609cbbeb95b557c503c6e4e5e9c');
+        $I->sendGET('/1/post/view/1');
+        $I->seeResponseCodeIs(200);
+        $I->seeResponseCodeIsSuccessful();
+        $I->seeResponseIsJson();
+        $I->seeResponseContainsJson(['title' => "Weshop"]);
     }
 
     public function testGetNotFound(ApiTester $I)
     {
         $I->haveHttpHeader('Content-Type','application/json');
-        $I->haveHttpHeader('X-Access-Token','c2c41e1d66e70e3b671182a0fc6eca56');
-        $I->sendGET('/1/post/100');
-        $I->seeResponseCodeIs(404);
-        $I->seeResponseCodeIsSuccessful();
+        $I->haveHttpHeader('X-Access-Token','3fea2609cbbeb95b557c503c6e4e5e9c');
+        $I->sendGET('/1/post/view/100');
+        $I->seeResponseCodeIs(400);
         $I->seeResponseIsJson();
-        $I->seeResponseContainsJson(['name' => 'Not Found']);
+        $I->seeResponseContainsJson(["errors"=> "Invalid Record requested"]);
     }
 
     public function testCreate(ApiTester $I)
     {
         $I->haveHttpHeader('Content-Type','application/json');
-        $I->haveHttpHeader('X-Access-Token','c2c41e1d66e70e3b671182a0fc6eca56');
-        $I->sendPOST('/1/post', [
+        $I->haveHttpHeader('X-Access-Token','3fea2609cbbeb95b557c503c6e4e5e9c');
+        $I->sendPOST('/1/post/create', [
             'title' => 'Test Title',
             'text' => 'Test Text',
         ]);
-        $I->seeResponseCodeIs(201);
+        $I->seeResponseCodeIs(200);
         $I->seeResponseCodeIsSuccessful();
         $I->seeResponseIsJson();
         $I->seeResponseContainsJson(['title' => 'Test Title']);
@@ -68,24 +69,24 @@ class PostsApiCest
     public function testUpdate(ApiTester $I)
     {
         $I->haveHttpHeader('Content-Type','application/json');
-        $I->haveHttpHeader('X-Access-Token','c2c41e1d66e70e3b671182a0fc6eca56');
-        $I->sendPUT('/1/post/2', [
-            'title' => 'New Title',
+        $I->haveHttpHeader('X-Access-Token','3fea2609cbbeb95b557c503c6e4e5e9c');
+        $I->sendPUT('/1/post/update/4', [
+            'title' => 'New Title Weshop 2019',
         ]);
         $I->seeResponseCodeIs(200);
         $I->seeResponseCodeIsSuccessful();
         $I->seeResponseIsJson();
         $I->seeResponseContainsJson([
-            'title' => 'New Title',
-            'text' => 'Old Text For Updating',
+            'title' => 'New Title Weshop 2019',
+            //'text' => 'Old Text For Updating',
         ]);
     }
 
     public function testDelete(ApiTester $I)
     {
         $I->haveHttpHeader('Content-Type','application/json');
-        $I->haveHttpHeader('X-Access-Token','c2c41e1d66e70e3b671182a0fc6eca56');
-        $I->sendDELETE('/1/post/3');
+        $I->haveHttpHeader('X-Access-Token','3fea2609cbbeb95b557c503c6e4e5e9c');
+        $I->sendDELETE('/1/post/delete/6');
         $I->seeResponseCodeIs(204);
     }
 }
