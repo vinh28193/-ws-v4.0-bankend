d13bb4b803872f28503b91d3f85a99ea24440d43
Hoang Cong Phuc <phuchc@peacesoft.net>
2019-03-13 20:59:49 +0700

    update code gen data

---
+++ common/fixtures/data/data_fixed/store.php
@@ -0,0 +1,74 @@
+<?php
+
+return [
+    'store0' => [
+        'id' => 1,
+        'country_id' => 6,
+        'locale' => 'US',
+        'name' => 'United State',
+        'country_name' => 'United State',
+        'address' => '86 Phố Nhiên, Phường Dương, Quận Trình Hoa Hoàng
+Hải Phòng',
+        'url' => 'http://www.bac.info/nesciunt-aspernatur-totam-et-est-soluta',
+        'currency' => 'RMB',
+        'currency_id' => 2,
+        'status' => 1,
+        'env' => 2,
+    ],
+    'store1' => [
+        'id' => 2,
+        'country_id' => 9,
+        'locale' => 'CA',
+        'name' => 'Mauritania',
+        'country_name' => 'Mauritania',
+        'address' => '870 Phố Khúc Tú Trang, Phường Ung, Huyện Dân Tiến
+Gia Lai',
+        'url' => 'http://bo.info/',
+        'currency' => 'USD',
+        'currency_id' => 3,
+        'status' => 1,
+        'env' => 0,
+    ],
+    'store2' => [
+        'id' => 3,
+        'country_id' => 10,
+        'locale' => 'DM',
+        'name' => 'Portugal',
+        'country_name' => 'Portugal',
+        'address' => '472 Phố Nông, Xã Ca Chế, Quận Mã Giang Thụy
+Hồ Chí Minh',
+        'url' => 'http://tu.biz.vn/totam-non-quis-illo-doloremque-saepe-dolorem-molestias',
+        'currency' => 'EUR',
+        'currency_id' => 7,
+        'status' => 0,
+        'env' => 2,
+    ],
+    'store3' => [
+        'id' => 4,
+        'country_id' => 4,
+        'locale' => 'sg',
+        'name' => 'Singapore',
+        'country_name' => 'Singapore',
+        'address' => '08 Phố Vừ, Ấp Nguyễn Khê, Huyện 13
+Hải Phòng',
+        'url' => 'https://van.info.vn/aliquid-quidem-ad-animi-neque-molestiae-laborum-et-maxime.html',
+        'currency' => 'EUR',
+        'currency_id' => 7,
+        'status' => 1,
+        'env' => 1,
+    ],
+    'store4' => [
+        'id' => 5,
+        'country_id' => 4,
+        'locale' => 'sg',
+        'name' => 'Singapore',
+        'country_name' => 'Singapore',
+        'address' => '20 Phố Minh, Phường 9, Huyện Vi Khánh
+Vĩnh Phúc',
+        'url' => 'http://www.bac.biz.vn/',
+        'currency' => 'JPY',
+        'currency_id' => 4,
+        'status' => 1,
+        'env' => 1,
+    ],
+];
