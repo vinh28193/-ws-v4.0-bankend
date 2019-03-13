c7986b7fb33dd665a165d78bf16333692d49ed29
Hoang Cong Phuc <phuchc@peacesoft.net>
2019-03-13 16:08:36 +0700

    update gen data fixed not null + phu thuoc

---
+++ common/fixtures/data/data_fixed/warehouse.php
@@ -0,0 +1,44 @@
+<?php
+
+return [
+    'warehouse0' => [
+        'id' => 1,
+        'name' => 'Dr. Stacey Upton',
+        'description' => 'Wayne Nikolaus',
+        'district_id' => null,
+        'province_id' => 2,
+        'country_id' => 1,
+        'store_id' => 1,
+        'address' => '6869 Adams Garden Apt. 385
+East Shanny, SC 04240',
+        'type' => 2,
+        'warehouse_group' => 1,
+        'post_code' => '49266-6992',
+        'telephone' => '(532) 938-1527 x356',
+        'email' => 'pupton@hayes.com',
+        'contact_person' => '',
+        'ref_warehouse_id' => 95,
+        'created_at' => 149546701,
+        'updated_at' => 931489003,
+    ],
+    'warehouse1' => [
+        'id' => 2,
+        'name' => 'Maryse Lehner',
+        'description' => 'Carolina Brakus Jr.',
+        'district_id' => null,
+        'province_id' => 2,
+        'country_id' => 1,
+        'store_id' => 1,
+        'address' => '387 Prosacco Harbors
+Lake Greggfort, WI 02445',
+        'type' => 2,
+        'warehouse_group' => 1,
+        'post_code' => '71926-8335',
+        'telephone' => '830.326.7993 x3336',
+        'email' => 'balistreri.adele@harris.com',
+        'contact_person' => '',
+        'ref_warehouse_id' => 30,
+        'created_at' => 1316340525,
+        'updated_at' => 182830963,
+    ],
+];
