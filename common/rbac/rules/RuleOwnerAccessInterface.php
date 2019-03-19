681d6f6426fa1a444a2185eebc86f5e0a9622616
Vinh (envy) <vinhvv@peacesoft.net>
2019-03-19 13:39:35 +0700

    rbac

--- common/rbac/rules/RuleOwnerAccessInterface.php
+++ common/rbac/rules/RuleOwnerAccessInterface.php
@@ -11,9 +11,20 @@ namespace common\rbac\rules;
 interface RuleOwnerAccessInterface
 {
     /**
-     * parameters for [[CheckAccessInterface::checkAccess()]]
+     *   class Order implements RuleOwnerAccessInterface{
+     *      public function getRuleParams(){
+                return $this->saleAssignId;
+     *      }
+     *  }
+     *  in controller
+     *  $order = Order::findOne(1);
+     *  if(this->can('Permission',$order)){
+     *      // action
+     *  }
+     *  --
+     * rule parameters for [[CheckAccessInterface::checkAccess()]]
      * @see \yii\web\User::can()
-     * @return array
+     * @return array|string|integer
      */
     public function getRuleParams();
 }
