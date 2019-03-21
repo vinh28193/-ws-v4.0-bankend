8f331d0aeb1d3270a71980051f4df86606d778ae
Vinh (envy) <vinhvv@peacesoft.net>
2019-03-21 21:00:30 +0700

    tracking

--- api/modules/v1/controllers/TrackingCodeController.php
+++ api/modules/v1/controllers/TrackingCodeController.php
@@ -10,11 +10,9 @@ namespace api\modules\v1\controllers;
 
 
 use api\controllers\BaseApiController;
+use common\data\ActiveDataProvider;
 use common\models\TrackingCode;
 use Yii;
-use yii\db\Expression;
-use yii\db\Query;
-use yii\helpers\ArrayHelper;
 
 class TrackingCodeController extends BaseApiController
 {
@@ -48,41 +46,21 @@ class TrackingCodeController extends BaseApiController
      */
     public function actionIndex()
     {
-
-        $start = microtime(true);
         $queryParams = Yii::$app->request->getQueryParams();
         $query = TrackingCode::find();
-        $query->filter($queryParams);
-        $cloneQuery = (clone $query)->limit(-1)->offset(-1)->orderBy([]);
-        $totalCount = $cloneQuery->select([new Expression('`tracking_code`.``id`')])->count(new Expression('`tracking_code`.`id`'));
-        $page = ArrayHelper::getValue($queryParams, 'p', 1);
-        $pageSize = ArrayHelper::getValue($queryParams, 'ps', 20);
-        $totalPage = (int)(($totalCount + $pageSize - 1) / $pageSize);
-        $offset = ($page - 1) * $pageSize;
-
-        $summaryQuery = new Query();
-        $summaryQuery->from(['c' => $cloneQuery->select(['seller_weight', 'seller_quantity', 'local_warehouse_weight', 'local_warehouse_quantity'])]);
-        $summaryQuery->select([
-            'totalSellerWeight' => new Expression('SUM(`c`.`seller_weight`)'),
-            'totalSellerQuantity' => new Expression('SUM(`c`.`seller_quantity`)'),
-            'totalWarehouseWeight' => new Expression('SUM(`c`.`local_warehouse_weight`)'),
-            'totalWarehouseQuantity' => new Expression('SUM(`c`.`local_warehouse_quantity`)')
-        ]);
-        $summary = $summaryQuery->all(TrackingCode::getDb());
-        $query->limit($pageSize)->offset($offset);
-        $data = [
-            '_items' => $query->all(),
-            '_meta' => [
-                'totalCount' => $totalCount,
-                'pageCount' => $totalPage,
-                'currentPage' => $page,
-                'perPage' => $pageSize
+        $dataProvider = new ActiveDataProvider([
+            'query' => $query,
+            'pagination' => [
+                'pageSizeParam' => 'ps',
+                'pageParam' => 'p',
+                'params' => $queryParams,
             ],
-            '_summary' => $summary
-        ];
-        $time = microtime(true) - $start;
-        Yii::info("action execute time: " . sprintf('%.3f', $time), __METHOD__);
-        return $this->response(true, 'Ok', $data);
+            'sort' => [
+                'params' => $queryParams,
+            ],
+        ]);
+
+        return $this->response(true, 'Ok', $dataProvider);
 
     }
 }
