35f1c72a299d30e19d290ea010178533b4c389c7
Hoang Cong Phuc <phuchc@peacesoft.net>
2019-03-22 22:56:03 +0700

    update

---
+++ console/mongodb-migrations/m190322_105151_Action_log_40_tables.php
@@ -0,0 +1,16 @@
+<?php
+
+class m190322_105151_Action_log_40_tables extends \yii\mongodb\Migration
+{
+    public function up()
+    {
+
+    }
+
+    public function down()
+    {
+        echo "m190322_105151_Action_log_40_tables cannot be reverted.\n";
+
+        return false;
+    }
+}
