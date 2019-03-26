f435f456e1a2ee48f9df9ea7cfd12e97ea197422
Vinh (envy) <vinhvv@peacesoft.net>
2019-03-26 10:52:06 +0700

    system localtion

--- api/modules/v1/controllers/SystemStateProvinceController.php
+++ api/modules/v1/controllers/SystemStateProvinceController.php
@@ -10,54 +10,21 @@ namespace api\modules\v1\controllers;
 
 
 use api\controllers\BaseApiController;
+use common\models\SystemCountry;
 use Yii;
 
 class SystemStateProvinceController extends BaseApiController
 {
-    public function behaviors()
-    {
-        $behaviors = parent::behaviors();
-//        $behaviors['pageCache'] = [
-//            'class' => 'yii\filters\PageCache',
-//            'only' => ['index'],
-//            'duration' => 24 * 3600 * 365, // 1 year
-//            'dependency' => [
-//                'class' => 'yii\caching\ChainedDependency',
-//                'dependencies' => [
-//                    new DbDependency(['sql' => 'SELECT MAX(id) FROM ' . Order::tableName()])
-//                ]
-//            ],
-//        ];
-        return $behaviors;
-    }
-
 
     protected function rules()
     {
         return [
             [
                 'allow' => true,
-                'actions' => ['index', 'view', 'create', 'update'],
+                'actions' => ['index'],
                 'roles' => $this->getAllRoles(true),
 
-            ],
-            [
-                'allow' => true,
-                'actions' => ['view'],
-                'roles' => $this->getAllRoles(true),
-                'permissions' => ['canView']
-            ],
-            [
-                'allow' => true,
-                'actions' => ['create'],
-                'roles' => $this->getAllRoles(true, 'user'),
-                'permissions' => ['canCreate']
-            ],
-            [
-                'allow' => true,
-                'actions' => ['update', 'delete'],
-                'roles' => $this->getAllRoles(true, 'user'),
-            ],
+            ]
         ];
     }
 
@@ -65,26 +32,14 @@ class SystemStateProvinceController extends BaseApiController
     {
         return [
             'index' => ['GET', 'POST'],
-            'create' => ['POST'],
-            'update' => ['PATCH', 'PUT'],
-            'view' => ['GET'],
-            'delete' => ['DELETE']
         ];
     }
 
-    public function actionIndex($country = null) {
-        $cache = Yii::$app->cache;
-        $cacheKey = 'SAVE_CACHE_C_' . $country;
-        if (($data = $cache->get($cacheKey)) === false) {
-            $query = \common\models\db\SystemStateProvince::find()->with('systemDistricts');
-            if ($country !== 'all') {
-                $query->andWhere(['country_id' => $country]);
-            }
-            $data = $query->asArray()->all();
-            $cache->set($cacheKey, $data, 3600);
-        }
-        Yii::$app->response->format = 'json';
-        return $data;
+    public function actions()
+    {
+        return array_merge(parent::actions(),[
+            'index' => \common\actions\SystemLocationAction::className()
+        ]);
     }
 
 }
--- common/models/db/SystemCountry.php
+++ common/models/db/SystemCountry.php
@@ -14,6 +14,14 @@ use Yii;
  * @property string $language Nếu có nhiều , viết cách nhau bằng dấu phẩy
  * @property string $status
  * @property string $version version 4.0
+ *
+ * @property Address[] $addresses
+ * @property Order[] $orders
+ * @property Shipment[] $shipments
+ * @property Store[] $stores
+ * @property SystemDistrict[] $systemDistricts
+ * @property SystemStateProvince[] $systemStateProvinces
+ * @property Warehouse[] $warehouses
  */
 class SystemCountry extends \common\components\db\ActiveRecord
 {
@@ -49,5 +57,70 @@ class SystemCountry extends \common\components\db\ActiveRecord
             'status' => 'Status',
             'version' => 'Version',
         ];
+    }
+
+    /**
+     * @return \yii\db\ActiveQuery
+     */
+    public function getAddresses()
+    {
+        return $this->hasMany(Address::className(), ['country_id' => 'id']);
+    }
+
+    /**
+     * @return \yii\db\ActiveQuery
+     */
+    public function getOrders()
+    {
+        return $this->hasMany(Order::className(), ['receiver_country_id' => 'id']);
+    }
+
+    /**
+     * @return \yii\db\ActiveQuery
+     */
+    public function getShipments()
+    {
+        return $this->hasMany(Shipment::className(), ['receiver_country_id' => 'id']);
+    }
+
+    /**
+     * @return \yii\db\ActiveQuery
+     */
+    public function getStores()
+    {
+        return $this->hasMany(Store::className(), ['country_id' => 'id']);
+    }
+
+    /**
+     * @return \yii\db\ActiveQuery
+     */
+    public function getSystemDistricts()
+    {
+        return $this->hasMany(SystemDistrict::className(), ['country_id' => 'id']);
+    }
+
+    /**
+     * @return \yii\db\ActiveQuery
+     */
+    public function getSystemStateProvinces()
+    {
+        return $this->hasMany(SystemStateProvince::className(), ['country_id' => 'id']);
+    }
+
+    /**
+     * @return \yii\db\ActiveQuery
+     */
+    public function getWarehouses()
+    {
+        return $this->hasMany(Warehouse::className(), ['country_id' => 'id']);
+    }
+
+    /**
+     * {@inheritdoc}
+     * @return \common\models\queries\SystemStateProvinceQuery the active query used by this AR class.
+     */
+    public static function find()
+    {
+        return new \common\models\queries\SystemStateProvinceQuery(get_called_class());
     }
 }
