9c644d633391807b92418ec6032a6e4d3a45708e
huyvd <huyvd@peacesoft.net>
2019-03-27 13:15:02 +0700

    fix-order

--- api/modules/v1/controllers/RestWalletLogWsController.php
+++ api/modules/v1/controllers/RestWalletLogWsController.php
@@ -146,7 +146,7 @@ class RestWalletLogWsController extends BaseApiController
     {
         if ($id !== null) {
             $response = WalletLogWs::find()
-                ->where(['id' => $id])
+                ->where(['action_path' => $id])
                 ->asArray()->all();
             return $this->response(true, "Get  $id success", $response);
         } else {
