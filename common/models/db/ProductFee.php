ca6843519f87a99d913af255c4c9c0fce34619fe
Vinh (envy) <vinhvv@peacesoft.net>
2019-03-28 14:08:48 +0700

    gii generate

---
+++ common/models/db/ProductFee.php
@@ -0,0 +1,78 @@
+<?php
+
+namespace common\models\db;
+
+use Yii;
+
+/**
+ * This is the model class for table "product_fee".
+ *
+ * @property int $id
+ * @property int $order_id order id
+ * @property int $product_id Product Id
+ * @property string $type loại phí: đơn giá gốc, ship us, ship nhật , weshop fee, ...
+ * @property string $name
+ * @property string $amount Amount
+ * @property string $local_amount Local amount
+ * @property string $discount_amount Discount of type fee
+ * @property string $currency loại tiền ngoại tệ
+ * @property string $created_at
+ * @property string $updated_at
+ * @property int $remove
+ * @property string $version version 4.0
+ */
+class ProductFee extends \common\components\db\ActiveRecord
+{
+    /**
+     * {@inheritdoc}
+     */
+    public static function tableName()
+    {
+        return 'product_fee';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function rules()
+    {
+        return [
+            [['order_id', 'product_id', 'created_at', 'updated_at', 'remove'], 'integer'],
+            [['product_id', 'name'], 'required'],
+            [['amount', 'local_amount', 'discount_amount'], 'number'],
+            [['type', 'currency', 'version'], 'string', 'max' => 255],
+            [['name'], 'string', 'max' => 60],
+        ];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => 'ID',
+            'order_id' => 'Order ID',
+            'product_id' => 'Product ID',
+            'type' => 'Type',
+            'name' => 'Name',
+            'amount' => 'Amount',
+            'local_amount' => 'Local Amount',
+            'discount_amount' => 'Discount Amount',
+            'currency' => 'Currency',
+            'created_at' => 'Created At',
+            'updated_at' => 'Updated At',
+            'remove' => 'Remove',
+            'version' => 'Version',
+        ];
+    }
+
+    /**
+     * {@inheritdoc}
+     * @return \common\models\queries\ProductFeeQuery the active query used by this AR class.
+     */
+    public static function find()
+    {
+        return new \common\models\queries\ProductFeeQuery(get_called_class());
+    }
+}
