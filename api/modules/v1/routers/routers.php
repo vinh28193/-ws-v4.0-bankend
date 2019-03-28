a954ed93d71fc96350ecf16425ec0195c2f45009
huyvd <huyvd@peacesoft.net>
2019-03-28 14:58:11 +0700

    fix

--- api/modules/v1/routers/routers.php
+++ api/modules/v1/routers/routers.php
@@ -371,4 +371,25 @@ return [
         ],
         'extraPatterns' => []
     ],
+
+    [
+        'class' => \common\filters\ApiUrlRule::className(),
+        'prefix' => 'v1',
+        'pluralize' => false,
+        'controller' => ['package-item' => 'package-item'],
+        'tokens' => [
+            '{id}' => '<id:\\w[\\w,]*>',
+            '{token}' => '<token:\\d[\\d,]*>',
+        ],
+        'patterns' => [
+            'GET,HEAD' => 'index',
+            'PUT,PATCH {id}' => 'update',
+            'DELETE {id}' => 'delete',
+            'GET,HEAD {id}' => 'view',
+            'POST' => 'create',
+            'OPTIONS {id}' => 'options',
+            'OPTIONS' => 'options',
+        ],
+        'extraPatterns' => []
+    ],
 ];
