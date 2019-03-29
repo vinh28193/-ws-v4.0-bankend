fc2649169d36cd094a079ad977db2a76a10548fd
huyvd <huyvd@peacesoft.net>
2019-03-29 10:45:49 +0700

    fix-order

--- api/modules/v1/controllers/OrderController.php
+++ api/modules/v1/controllers/OrderController.php
@@ -163,14 +163,14 @@ class OrderController extends BaseApiController
         $response = null;
         if (!$model->save()) {
             Yii::$app->wsLog->order->push($model->getScenario(), null, [
-                'id' => $id,
+                'id' => $model->id,
                 'request' => $this->post,
                 'response' => $model->getErrors()
             ]);
             return $this->response(false, $model->getFirstErrors());
         }
         Yii::$app->wsLog->order->push($model->getScenario(), null, [
-            'id' => $id,
+            'id' => $model->id,
             'request' => $this->post,
             'response' => $dirtyAttributes
         ]);
