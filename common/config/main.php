68b3487aa92478353dc0c0ce61ba942db06fe872
Hoang Cong Phuc <phuchc@peacesoft.net>
2019-03-29 10:03:20 +0700

    update total_fee_product_local

--- common/config/main.php
+++ common/config/main.php
@@ -45,11 +45,11 @@ return [
         'wsLog' => [
             'class' => 'common\components\log\Logging'
         ],
-        'cartws' => [
-            'class' => 'common\components\book\cart\Cart',
-            'storage' => [
-                'class' => 'common\components\book\cart\storage\SessionStorage',
-            ],
-        ],
+//        'cartws' => [
+//            'class' => 'common\components\book\cart\Cart',
+//            'storage' => [
+//                'class' => 'common\components\book\cart\storage\SessionStorage',
+//            ],
+//        ],
     ],
 ];
