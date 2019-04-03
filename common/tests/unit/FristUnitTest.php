a0c32b1e6c8f415077e52314193785ab7179ac47
Vinh (envy) <vinhvv@peacesoft.net>
2019-04-03 21:14:06 +0700

    pre test

---
+++ common/tests/unit/FristUnitTest.php
@@ -0,0 +1,26 @@
+<?php namespace common\tests\unit;
+
+use common\tests\UnitTestCase;
+
+class FristUnitTest extends UnitTestCase
+{
+    /**
+     * @var \common\tests\UnitTester
+     */
+    protected $tester;
+    
+    protected function _before()
+    {
+    }
+
+    protected function _after()
+    {
+    }
+
+    // tests
+    public function testSomeFeature()
+    {
+        var_dump(\Yii::$app);
+        die;
+    }
+}
