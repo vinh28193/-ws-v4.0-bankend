70248309d360201bcd6279a53eab7e12dcc4a77c
huyvd <huyvd@peacesoft.net>
2019-04-19 21:05:33 +0700

    fix-order

---
+++ userbackend/controllers/HomeController.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace userbackend\controllers;
+
+use Yii;
+use common\models\Order;
+use userbackend\models\HomeSearch;
+use yii\web\Controller;
+use yii\web\NotFoundHttpException;
+use yii\filters\VerbFilter;
+
+/**
+ * HomeController implements the CRUD actions for Order model.
+ */
+class HomeController extends Controller
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function behaviors()
+    {
+        return [
+            'verbs' => [
+                'class' => VerbFilter::className(),
+                'actions' => [
+                    'delete' => ['POST'],
+                ],
+            ],
+        ];
+    }
+
+    /**
+     * Lists all Order models.
+     * @return mixed
+     */
+    public function actionIndex()
+    {
+
+        return $this->render('index');
+    }
+}
