10b251605f667e7e9307d314db590c0b93ecffe7
huyvd <huyvd@peacesoft.net>
2019-04-19 10:59:08 +0700

    fix-order

---
+++ console/migrations/m190419_020953_add_column_table_order.php
@@ -0,0 +1,43 @@
+<?php
+
+use yii\db\Migration;
+
+/**
+ * Class m190419_020953_add_column_table_order
+ */
+class m190419_020953_add_column_table_order extends Migration
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function safeUp()
+    {
+        $this->addColumn('order','mark_supported','bigint(20) NULL');
+        $this->addColumn('order','ready_purchase','bigint(20) NULL');
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function safeDown()
+    {
+        echo "m190419_020953_add_column_table_order cannot be reverted.\n";
+
+        return false;
+    }
+
+    /*
+    // Use up()/down() to run migration code without a transaction.
+    public function up()
+    {
+
+    }
+
+    public function down()
+    {
+        echo "m190419_020953_add_column_table_order cannot be reverted.\n";
+
+        return false;
+    }
+    */
+}
