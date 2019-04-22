f05acb4ef16bf33f0ba8eba45c1e74b528e75437
huyvd <huyvd@peacesoft.net>
2019-04-22 15:11:16 +0700

    fix-order

---
+++ userbackend/views/order/index.php
@@ -0,0 +1,50 @@
+<?php
+
+use yii\helpers\Html;
+use yii\grid\GridView;
+
+/* @var $this yii\web\View */
+/* @var $searchModel userbackend\models\OrderSearch */
+/* @var $dataProvider yii\data\ActiveDataProvider */
+
+$this->title = 'Orders';
+$this->params['breadcrumbs'][] = $this->title;
+?>
+<div class="be-order">
+    <div class="be-table">
+        <table class="table">
+            <thead>
+            <tr>
+                <th scope="col">Sản phẩm</th>
+                <th scope="col">Mã đơn hàng</th>
+                <th scope="col">Ngày mua</th>
+                <th scope="col">Tổng tiền</th>
+                <th scope="col">Trang thái đơn hàng</th>
+            </tr>
+            </thead>
+            <tbody>
+            <?php foreach ($models  as $order) { ?>
+            <tr>
+                <td>
+                    <div class="product-info">
+                        <div class="thumb">
+<!--                            <img src="--><?php //$order->products->link_img ?><!--" alt=""/>-->
+                        </div>
+                        <div class="info">
+<!--                            <b>--><?php //$order->products->product_name ?><!--</b>-->
+<!--                            <span>--><?php //$order->products->quantity ?><!--</span>-->
+                        </div>
+                    </div>
+                </td>
+                <td><a href="#"><?php $order->ordercode ?></a></td>
+                <td>
+                    <b><?php $order->new ?></b>
+                </td>
+                <td><b class="total text-orange"></b></td>
+                <td><b><?php $order->current_status ?></b></td>
+            </tr>
+            <?php } ?>
+            </tbody>
+        </table>
+    </div>
+</div>
