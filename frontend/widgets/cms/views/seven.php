b94dc07115708e485ba0461ab96a580c43dfa7cd
Vinh (envy) <vinhvv@peacesoft.net>
2019-05-03 16:40:40 +0700

    ebay

--- frontend/widgets/cms/views/seven.php
+++ frontend/widgets/cms/views/seven.php
@@ -1,130 +1,60 @@
 <?php
 
+use yii\helpers\Html;
+use frontend\widgets\cms\ProductWidget;
+
+/* @var $this yii\web\View */
+/* @var $block array */
+/* @var $iphoneOld boolean */
+/* @var $categories array */
+/* @var $products array */
+/* @var $images array */
+/* @var $keywords string */
+
 ?>
 
-<div class="product-block ebay">
+<div class="product-block">
     <div class="container">
         <div class="title-box">
-            <div class="title">Đồng hồ</div>
+            <div class="title"><?= $block['name']; ?></div>
             <ul class="title-breadcrumb">
-                <li><a href="#">Thời trang</a></li>
-                <li><a href="#">Đồ gia đình</a></li>
-                <li><a href="#">Công nghệ</a></li>
-                <li><a href="#">Xe máy</a></li>
-                <li><a href="#">Đồ trẻ em</a></li>
+                <?php foreach ($categories as $category): ?>
+                    <li><a href="<?= $category['url']; ?>"><?= $category['local_name'] ?></a></li>
+                <?php endforeach; ?>
             </ul>
-            <a href="#" class="see-all">Xem tất cả</a>
+            <a href="<?= $block['url'] ?>" class="see-all">Xem tất cả</a>
         </div>
         <div class="pd-content">
             <div class="left">
-                <a href="#"><img src="https://static-v3.weshop.com.vn/uploadImages/502e53/502e537daeadf86228f7dcc4318b6af0.jpg" alt="" title=""/></a>
+                <a href="<?= $block['url'] ?>">
+                    <?= Html::img($block['image'] !== null ? $block['image'] : 'http://static.weshop.com.vn/upload/2/k/u/n/8/8/d/u/c/v/2.png', []); ?>
+                </a>
             </div>
             <div class="right">
                 <div class="right-1">
                     <ul>
-                        <li>
-                            <div class="item">
-                                <a href="#">
-                                    <div class="name">Perman Unique Mens Quartz Round Dial Case Clock PU...</div>
-                                    <div class="info">
-                                        <div class="price">12.000.000đ</div>
-                                        <div class="old-price">24.000.000đ</div>
-                                        <span class="sale-tag">30% OFF</span>
-                                    </div>
-                                    <div class="thumb">
-                                        <span><img src="https://i.ebayimg.com/00/s/MTI4MFg1NDY=/z/g5cAAOSwi1tZZBHX/$_1.JPG" alt="" title=""/></span>
-                                    </div>
-                                </a>
-                            </div>
-                        </li>
-                        <li>
-                            <div class="item">
-                                <a href="#">
-                                    <div class="name">Perman Unique Mens Quartz Round Dial Case Clock PU...</div>
-                                    <div class="info">
-                                        <div class="price">12.000.000đ</div>
-                                        <div class="old-price">24.000.000đ</div>
-                                        <span class="sale-tag">30% OFF</span>
-                                    </div>
-                                    <div class="thumb">
-                                        <span><img src="https://i.ebayimg.com/00/s/MTYwMFgxMjAw/z/yTIAAOSw1SJbeFrp/$_57.JPG" alt="" title=""/></span>
-                                    </div>
-                                </a>
-                            </div>
-                        </li>
-                        <li>
-                            <div class="item">
-                                <a href="#">
-                                    <div class="name">Perman Unique Mens Quartz Round Dial Case Clock PU...</div>
-                                    <div class="info">
-                                        <div class="price">12.000.000đ</div>
-                                        <div class="old-price">24.000.000đ</div>
-                                        <span class="sale-tag">30% OFF</span>
-                                    </div>
-                                    <div class="thumb">
-                                        <span><img src="https://i.ebayimg.com/00/s/MTAwMFgxMDAw/z/L7cAAOSwaCVcitqE/$_1.JPG" alt="" title=""/></span>
-                                    </div>
-                                </a>
-                            </div>
-                        </li>
-                        <li>
-                            <div class="item">
-                                <a href="#">
-                                    <div class="name">Perman Unique Mens Quartz Round Dial Case Clock PU...</div>
-                                    <div class="info">
-                                        <div class="price">12.000.000đ</div>
-                                        <div class="old-price">24.000.000đ</div>
-                                        <span class="sale-tag">30% OFF</span>
-                                    </div>
-                                    <div class="thumb">
-                                        <span><img src="https://i.ebayimg.com/00/s/NTAwWDM0OQ==/z/x1gAAOSw01JbdP2m/$_57.JPG" alt="" title=""/></span>
-                                    </div>
-                                </a>
-                            </div>
-                        </li>
+                        <?php
+                        foreach ($products as $key => $product) {
+                            if ($key < 4) {
+                                echo ProductWidget::widget([
+                                    'product' => $product,
+                                    'type' => ProductWidget::TYPE_RIGHT
+                                ]);
+                            }
+                        } ?>
                     </ul>
                 </div>
                 <div class="right-2">
                     <ul>
-                        <li>
-                            <div class="item">
-                                <div class="thumb">
-                                    <span><img src="https://i.ebayimg.com/00/s/MTYwMFgxMTIw/z/aj0AAOSwL05cWHQr/$_1.JPG" alt="" title=""/></span>
-                                </div>
-                                <div class="info">
-                                    <div class="name">Perman Unique Mens Quartz Round Dial Case Clock PU...</div>
-                                    <div class="price">12.000.000đ</div>
-                                    <div class="old-price">24.000.000đ</div>
-                                    <span class="sale-tag">30% OFF</span>
-                                </div>
-                            </div>
-                        </li>
-                        <li>
-                            <div class="item">
-                                <div class="thumb">
-                                    <span><img src="https://i.ebayimg.com/00/s/NjAwWDUwMA==/z/LL0AAOSwK65buxUy/$_1.JPG" alt="" title=""/></span>
-                                </div>
-                                <div class="info">
-                                    <div class="name">Perman Unique Mens Quartz Round Dial Case Clock PU...</div>
-                                    <div class="price">12.000.000đ</div>
-                                    <div class="old-price">24.000.000đ</div>
-                                    <span class="sale-tag">30% OFF</span>
-                                </div>
-                            </div>
-                        </li>
-                        <li>
-                            <div class="item">
-                                <div class="thumb">
-                                    <span><img src="https://i.ebayimg.com/00/s/MTYwMFg4ODg=/z/U-UAAOSwZjpcgrAS/$_1.JPG" alt="" title=""/></span>
-                                </div>
-                                <div class="info">
-                                    <div class="name">Perman Unique Mens Quartz Round Dial Case Clock PU...</div>
-                                    <div class="price">12.000.000đ</div>
-                                    <div class="old-price">24.000.000đ</div>
-                                    <span class="sale-tag">30% OFF</span>
-                                </div>
-                            </div>
-                        </li>
+                        <?php
+                        foreach ($products as $key => $product) {
+                            if ($key > 3 && $key < 7) {
+                                echo ProductWidget::widget([
+                                    'product' => $product,
+                                    'type' => ProductWidget::TYPE_LEFT
+                                ]);
+                            }
+                        } ?>
                     </ul>
                 </div>
             </div>
@@ -132,17 +62,26 @@
         <div class="pd-tag-box">
             <div class="tag-box box-1">
                 <div class="title">Từ khóa tìm kiếm nhiều</div>
-                <div class="keyword"><a href="#">đồng hồ đeo tay</a>, <a href="#">rolex</a>, <a href="#">seiko</a>, <a href="#">casio</a>, <a href="#">đồng hồ cơ</a>, <a href="#">đồng hồ thông minh</a></div>
+                <div class="keyword">
+                    <?php
+                        foreach (explode(',',$keywords) as $keyword){
+                            echo Html::a($keyword,'#');
+                        }
+                    ?>
+                    <a href="#">đồng hồ đeo tay</a>, <a href="#">rolex</a>, <a href="#">seiko</a>, <a
+                            href="#">casio</a>, <a href="#">đồng hồ cơ</a>, <a href="#">đồng hồ thông minh</a></div>
             </div>
             <div class="brand-list">
                 <ul>
-                    <li><a href="#"><img src="https://static-v3.weshop.com.vn/uploadImages/a3d24c/a3d24c76e87178fc0bcba6aae7cbc2de.png" alt="" title=""/></a></li>
-                    <li><a href="#"><img src="https://static-v3.weshop.com.vn/uploadImages/661ac8/661ac83cf6b99b0e289c21aee279e88a.png" alt="" title=""/></a></li>
-                    <li><a href="#"><img src="https://static-v3.weshop.com.vn/uploadImages/517c49/517c4959a48599f38132c614e37999a2.png" alt="" title=""/></a></li>
-                    <li><a href="#"><img src="https://static-v3.weshop.com.vn/uploadImages/1b9519/1b9519dad597ce25d245a34c57335b82.png" alt="" title=""/></a></li>
-                    <li><a href="#"><img src="https://static-v3.weshop.com.vn/uploadImages/d421a4/d421a4fa057d5ea1aa4a3a38a8e02511.png" alt="" title=""/></a></li>
-                    <li><a href="#"><img src="https://static-v3.weshop.com.vn/uploadImages/9a6cdc/9a6cdca969a8c16edf486b3330531613.png" alt="" title=""/></a></li>
-                    <li><a href="#"><img src="https://static-v3.weshop.com.vn/uploadImages/d0c1f5/d0c1f524979cb8c01fe00fcb42a6d54f.png" alt="" title=""/></a></li>
+                    <?php foreach ($images as $image): ?>
+                        <li>
+                            <a href="<?= $image['link'] ?>">
+                                <img src="<?= $image['domain'] ?><?= $image['origin_src'] ?>"
+                                        alt="<?= $image['name'] ?>"
+                                        title="<?= $image['name'] ?>">
+                            </a>
+                        </li>
+                    <?php endforeach; ?>
                 </ul>
             </div>
         </div>
