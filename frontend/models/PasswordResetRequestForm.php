b8786955d3ff79178654a3dc905bb2ce883466f1
Hoang Cong Phuc <phuchc@peacesoft.net>
2019-05-15 11:08:50 +0700

    reset pass user customer

--- frontend/models/PasswordResetRequestForm.php
+++ frontend/models/PasswordResetRequestForm.php
@@ -59,7 +59,8 @@ class PasswordResetRequestForm extends Model
                 ['html' => 'passwordResetToken-html', 'text' => 'passwordResetToken-text'],
                 ['user' => $user]
             )
-            ->setFrom([Yii::$app->params['supportEmail'] => Yii::$app->name . ' robot'])
+            //->setFrom([Yii::$app->params['supportEmail'] => Yii::$app->name . ' robot'])
+            ->setFrom([Yii::$app->params['supportEmail'] => 'Weshop Việt Nam robot'])
             ->setTo($this->email)
             ->setSubject('Password reset for ' . Yii::$app->name)
             ->send();
