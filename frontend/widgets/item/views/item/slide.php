8a0e4440635a22eb610a5d4d6679d2bd2838dd6b
VienLV <VienLV@example.com>
2019-05-15 09:58:22 +0700

    fix img

--- frontend/widgets/item/views/item/slide.php
+++ frontend/widgets/item/views/item/slide.php
@@ -15,7 +15,7 @@ use yii\helpers\Html;
                 foreach ($item->primary_images as $image){ ?>
                     <div class="item">
                         <a href="javascript:void (0);" onclick="changeBigImage(this)" data-image="<?= $image->main ?>" data-zoom-image="<?= $image->main ?>">
-                            <img src="<?= $image->main ?>" width="100"/>
+                            <img src="<?= $image->main ?>" style="max-width: 100px"/>
                         </a>
                     </div>
                 <?php }
@@ -26,7 +26,7 @@ use yii\helpers\Html;
             <img id="detail-big-img" class="detail-big-img"
                  src="<?= $item->primary_images ? $item->primary_images[0]->main : '/img/no_image.png' ?>"
                  data-zoom-image="<?= $item->primary_images ? $item->primary_images[0]->main : '/img/no_image.png' ?>"
-                 style="height: inherit;"/>
+                 style="max-width: 400px;max-height: 400px;position: absolute;"/>
         <!--<div class="out-of-date">-->
         <!--<span><i class="fas fa-exclamation-triangle"></i><br/>Hàng hết hạn bán</span>-->
         <!--</div>-->
