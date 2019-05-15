84107ebf292af0cc6bef2ab01bfe495f2cdc2351
Hoang Cong Phuc <phuchc@peacesoft.net>
2019-05-15 21:42:37 +0700

    update login front end

--- common/components/AuthCustomerHandler.php
+++ common/components/AuthCustomerHandler.php
@@ -30,7 +30,15 @@ class AuthCustomerHandler
         $attributes = $this->client->getUserAttributes();
         $email = ArrayHelper::getValue($attributes, 'email');
         $id = ArrayHelper::getValue($attributes, 'id');
-        $nickname = ArrayHelper::getValue($attributes, 'login');
+        if(ArrayHelper::index($attributes, 'login')){
+            $nickname = ArrayHelper::getValue($attributes, 'login');
+        }else if(ArrayHelper::index($attributes, 'name')){
+            $nickname = ArrayHelper::getValue($attributes, 'name');
+        }
+
+        $email_verified = ArrayHelper::getValue($attributes, 'email_verified');
+        $picture = ArrayHelper::getValue($attributes, 'picture');
+
         $duration = isset(Yii::$app->params['user.rememberMeDuration']) ? isset(Yii::$app->params['user.rememberMeDuration']) : 3600;
 
         /* @var Auth $auth */
@@ -46,32 +54,59 @@ class AuthCustomerHandler
                 ]);
             } else {
                 $password = Yii::$app->security->generateRandomString(6);
-                $user = new Customer([
+                $Customer = new Customer([
                     'username' => $nickname ? $nickname : $email,
-//                        'github' => $nickname,
                     'email' => $email,
                     'password' => $password,
                     'updated_at'=> time(),
+                    'store_id' => 1, // Domain Weshop Viet Nam
+                    'active_shipping' => 0,
+                    'total_xu' => 0,
+                    'usable_xu' => 0 ,
+                    'last_revenue_xu' => 0 ,
+                    'email_verified' => $email_verified ? $email_verified : 0 ,  // Nếu là Google Email lấy được rồi nên coi như là dã xác nhận
+                    'phone_verified' => 0, // Vì là đăng kí qua Google + Facebook nên chưa xác nhân hoặc lấy được số đt của khách hàng
+                    'gender' => 0 ,
+                    'type_customer' => 1 , // set 1 là Khách Lẻ và 2 là Khách buôn - WholeSale Customer
+                    'avatar' => $picture ? $picture : null ,
+                    'note_by_employee' => 'Khách hàng tạo tài khoản qua '. $this->client->getId() .' .Attributes ' . serialize($attributes),
                 ]);
 
-                $user->generateAuthKey();
-                $user->generatePasswordResetToken();
+                $Customer->generateAuthKey();
+                $Customer->generatePasswordResetToken();
 
                 $transaction = Customer::getDb()->beginTransaction();
 
-                if ($user->save()) {
-                    $transaction->commit();
-                    Yii::$app->user->login($user, $duration);
+                if ($Customer->save()) {
+                    $auth = new Auth([
+                        'user_id' => $Customer->id,
+                        'source' => $this->client->getId(),
+                        'source_id' => (string)$id,
+                    ]);
+
+                    if ($auth->save()) {
+                        $transaction->commit();
+                        Yii::$app->user->login($Customer, $duration);
+                    } else {
+                        Yii::$app->getSession()->setFlash('error', [
+                            Yii::t('app', 'Unable to save {client} account: {errors}', [
+                                'client' => $this->client->getTitle(),
+                                'errors' => json_encode($auth->getErrors()),
+                            ]),
+                        ]);
+                    }
+
                 } else {
                     Yii::$app->getSession()->setFlash('error', [
-                        Yii::t('app', 'Unable to save user: {errors}'),
+                        Yii::t('app', 'Unable to save Customer : {errors}'),
                     ]);
                 }
             }
         } else {
             Yii::$app->getSession()->setFlash('success', [
                 Yii::t('app', 'Linked {client} account.', [
-                    'client' => $this->client->getTitle()
+                    'client' => $this->client->getTitle(),
+                    'errors' => json_encode($user->getErrors()),
                 ]),
             ]);
         }
