2c335b9f1a58d936c813cbc245f836fe5e3bf540
Hoang Cong Phuc <phuchc@peacesoft.net>
2019-05-16 13:54:59 +0700

    update code

--- frontend/models/LoginForm.php
+++ frontend/models/LoginForm.php
@@ -14,7 +14,7 @@ class LoginForm extends Model
 
     public $email;
     public $password;
-    public $rememberMe = false;
+    public $rememberMe = true;
 
     private $_user;
 
@@ -71,13 +71,14 @@ class LoginForm extends Model
     /**
      * Finds user by [[email]]
      *
-     * @return Customer|null
+     * @return User|null
      */
     protected function getUser()
     {
         if ($this->_user === null) {
-            $this->_user = User::findAdvance($this->email);
+            $this->_user = User::findByEmail($this->email);
         }
+
         return $this->_user;
     }
 }
--- frontend/views/secure/login.php
+++ frontend/views/secure/login.php
@@ -1,5 +1,9 @@
 <?php
 
+/* @var $this yii\web\View */
+/* @var $form yii\bootstrap\ActiveForm */
+/* @var $model frontend\models\LoginForm */
+
 use yii\helpers\Html;
 use yii\bootstrap4\ActiveForm;
 use yii\authclient\widgets\AuthChoice;
@@ -11,53 +15,45 @@ $this->title = 'Đăng nhập';
 echo Html::tag('div',Html::tag('span',$this->title),['class' => 'title'])
 ?>
 
+<p>Please fill out the following fields to login:</p>
 
-<?php $form = ActiveForm::begin(['id' => 'login-form', 'options' => [
-    'class' => 'payment-form'
-]
-]); ?>
-<div class="form-group">
-    <?= $form->field($model, 'email',['template' => " <i class=\"icon email\"></i>{input}\n{hint}\n{error}"])->textInput()->input('text', ['placeholder' => " Nhập Email"])?>
-</div>
-<div class="form-group">
-    <?= $form->field($model, 'password', ['template' => "<i class=\"icon password\"></i>{input}\n{hint}\n{error}"])->passwordInput(['placeholder' => "Mật khẩu"]) ?>
-</div>
-<div class="check-info">
-    <?= $form->field($model, 'rememberMe')->checkbox(['class' => 'form-check-input']) ?>
-    <div style="color:#999;margin:1em 0">
-        <?= Html::a('Quên mật khẩu ?', ['secure/request-password-reset']) ?>
+
+    <?php $form = ActiveForm::begin(['id' => 'login-form', 'options' => [ 'class' => 'payment-form']]); ?>
+    <div class="form-group">
+        <?= $form->field($model, 'email',['template' => " <i class=\"icon email\"></i>{input}\n{hint}\n{error}"])->textInput(['autofocus' => true])->input('text', ['placeholder' => " Nhập Email"])?>
     </div>
-</div>
-
-<div class="form-group">
-    <?= Html::submitButton('Login', ['class' => 'btn btn-primary', 'name' => 'login-button']) ?>
-</div>
-
-<?php ActiveForm::end(); ?>
-<div class="other-login">
-    <div class="text-center"><span class="or">Hoặc đăng nhâp qua</span></div>
-    <div class="social-button-ws">
-
-        <?php $authAuthChoice = AuthChoice::begin([  'baseAuthUrl' => ['secure/auth'] , 'popupMode' => false, ]); ?>
-                <div class="social-button">
-             <?php foreach ($authAuthChoice->getClients() as $client): ?>
-                 <?= $authAuthChoice->clientLink($client) ?>
-             <?php endforeach; ?>
-                </div>
-         <?php AuthChoice::end(); ?>
-
-        <?php
-        /*
-        =yii\authclient\widgets\AuthChoice::widget([
-            'baseAuthUrl' => ['secure/auth'],
-            'popupMode' => false,
-        ])
-        */
-        ?>
-
+    <div class="form-group">
+        <?= $form->field($model, 'password', ['template' => "<i class=\"icon password\"></i>{input}\n{hint}\n{error}"])->passwordInput(['placeholder' => "Mật khẩu"]) ?>
+    </div>
+    <div class="check-info">
+        <?= $form->field($model, 'rememberMe')->checkbox(['class' => 'form-check-input']) ?>
+        <div style="color:#999;margin:1em 0">
+            <?= Html::a('Quên mật khẩu ?', ['secure/request-password-reset']) ?>
+        </div>
     </div>
 
-    <p>Quý khách chưa có tài khoản
-        <?php echo Html::a('Đăng ký ngay', ['/secure/register']); ?>
-    </p>
-</div>
+    <div class="form-group">
+        <?= Html::submitButton('Login', ['class' => 'btn btn-primary', 'name' => 'login-button']) ?>
+    </div>
+
+    <?php ActiveForm::end(); ?>
+
+    <div class="other-login">
+        <div class="text-center"><span class="or">Hoặc đăng nhâp qua</span></div>
+        <div class="social-button-ws">
+
+            <?php $authAuthChoice = AuthChoice::begin([  'baseAuthUrl' => ['secure/auth'] , 'popupMode' => false, ]); ?>
+                    <div class="social-button">
+                 <?php foreach ($authAuthChoice->getClients() as $client): ?>
+                     <?= $authAuthChoice->clientLink($client) ?>
+                 <?php endforeach; ?>
+                    </div>
+             <?php AuthChoice::end(); ?>
+
+        </div>
+
+        <p>Quý khách chưa có tài khoản
+            <?php echo Html::a('Đăng ký ngay', ['/secure/register']); ?>
+        </p>
+    </div>
+
