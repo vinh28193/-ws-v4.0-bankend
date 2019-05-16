99b0b24dfc3386f42924778dcaa768f0e3838dcc
huyvd <huyvd@peacesoft.net>
2019-05-16 17:26:23 +0700

    fix

---
+++ userbackend/views/site/error.php
@@ -0,0 +1,16 @@
+<?php
+
+/* @var $this yii\web\View */
+/* @var $name string */
+/* @var $message string */
+/* @var $exception Exception */
+
+use yii\helpers\Html;
+
+?>
+<div class="content-404">
+    <span class="image"><img src="/img/404.png"></span>
+    <div class="link text-white">
+        Chúng tôi không thể tìm thấy trang bạn yêu cầu. Bạn có thể <a href="/" class="text-white">trở về trang chủ ngay bây giờ.</a>
+    </div>
+</div>
