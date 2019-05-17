aaaf394bb8fca06afda234b6338a4394a646171b
huyvd <huyvd@peacesoft.net>
2019-05-17 09:02:09 +0700

    fix

--- frontend/modules/account/views/home/index.php
+++ frontend/modules/account/views/home/index.php
@@ -114,22 +114,76 @@ use yii\helpers\Html;
     </div>
 </div>
 
-<div class="modal fade" id="exampleModalCenter" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
-    <div class="modal-dialog modal-dialog-centered" role="document">
-        <div class="modal-content">
-            <div class="modal-header">
-                <h5 class="modal-title" id="exampleModalLongTitle">Modal title</h5>
-                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                    <span aria-hidden="true">&times;</span>
-                </button>
-            </div>
-            <div class="modal-body">
-                ...
-            </div>
-            <div class="modal-footer">
-                <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-                <button type="button" class="btn btn-primary">Save changes</button>
-            </div>
-        </div>
-    </div>
-</div>
+<!--<div class="modal" id="exampleModalCenter" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">-->
+<!--    <div class="modal-dialog modal-dialog-centered" role="document">-->
+<!--        <div class="modal-content">-->
+<!--            <div class="row">-->
+<!--                <div class="col-lg-12 p-0">-->
+<!--                    <div class="card m-b-0">-->
+<!--                        <div class="card-body">-->
+<!--                            <div class="row">-->
+<!--                                <div class="col-lg-12 p-0">-->
+<!--                                    <div class="modal-body p-0">-->
+<!--                                        <div class="ng-star-inserted">-->
+<!--                                            <div class="col-md-12 m-0 col-xl-12 chat">-->
+<!--                                                <div class="card m-0 p-0">-->
+<!--                                                    <div class="card-header msg_head">-->
+<!--                                                        <div class="d-flex bd-highlight">-->
+<!--                                                            <div class="img_cont">-->
+<!--                                                                <img src="../../../../../assets/images/users/1.jpg" class="rounded-circle user_img">-->
+<!--                                                                <span class="online_icon"></span>-->
+<!--                                                            </div>-->
+<!--                                                            <div class="user_info">-->
+<!--                                                                <span style="color: black">Trao đổi với khách hàng</span>-->
+<!--                                                                <p style="color: black"></p>-->
+<!--                                                            </div>-->
+<!--                                                        </div>-->
+<!--                                                    </div>-->
+<!--                                                    <div class="card-body msg_card_body" #scrollMe [scrollTop]="scrollMe.scrollHeight">-->
+<!--                                                        <div>-->
+<!--                                                            <div>-->
+<!--                                                                <div class="d-flex justify-content-start mb-4">-->
+<!--                                                                    <div class="img_cont_msg">-->
+<!--                                                                        <img src="../../../../../assets/images/users/4.jpg"-->
+<!--                                                                             class="rounded-circle user_img_msg">-->
+<!--                                                                    </div>-->
+<!--                                                                    <div>-->
+<!--                                                                        <span class="msg_cotainer_send "></span><br>-->
+<!--                                                                        <div class="">-->
+<!--                                                                            <span class="mr-2">weshop</span>-->
+<!--                                                                            <span  class="text-darkgray">12345</span>-->
+<!--                                                                        </div>-->
+<!--                                                                    </div>-->
+<!--                                                                </div>-->
+<!--                                                            </div>-->
+<!--                                                        </div>-->
+<!--                                                    </div>-->
+<!--                                                    <div class="card-footer">-->
+<!--                                                        <div class="card-body">-->
+<!--                                                            <div class="input-group">-->
+<!--            <textarea name="" rows="4" class="form-control type_msg"-->
+<!--                      placeholder="Enter the exchange content.-->
+<!--Press Shift + Enter to get down the line.-->
+<!--Enter to send" i18n-placeholder></textarea>-->
+<!--                                                                <div class="input-group-btn">-->
+<!--                                                                    <button style="height: 80px;" class="btn btn-default">Sent</button>-->
+<!--                                                                </div>-->
+<!--                                                            </div>-->
+<!--                                                        </div>-->
+<!--                                                    </div>-->
+<!--                                                </div>-->
+<!--                                            </div>-->
+<!--                                        </div>-->
+<!--                                    </div>-->
+<!--                                </div>-->
+<!--                                <div class="col-md-12 text-right mt-2">-->
+<!--                                    <button class="btn btn-danger btn-sm" data-dismiss="modal" (click)="offModeChat()">Cancel</button>-->
+<!--                                </div>-->
+<!--                            </div>-->
+<!--                        </div>-->
+<!--                    </div>-->
+<!--                </div>-->
+<!--            </div>-->
+<!--        </div>-->
+<!--    </div>-->
+<!--</div>-->
