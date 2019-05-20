a4b084f08a173ca7a2a12e26a92de81e04a1115b
VienLV <VienLV@example.com>
2019-05-20 09:50:24 +0700

    fix bug asset

--- frontend/modules/account/views/wallet/index.php
+++ frontend/modules/account/views/wallet/index.php
@@ -1,4 +1,9 @@
 <?php
+
+use frontend\modules\account\views\widgets\HeaderContentWidget;
+
+$this->title = 'Ví WeShop Của Bạn';
+echo HeaderContentWidget::widget(['title' => 'Ví của bạn','stepUrl' => 'my-weshop/wallet.html']);
 ?>
 
-Index ví
+
