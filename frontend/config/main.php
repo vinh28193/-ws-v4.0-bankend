0e2549ec9cb92dfd9ac46c23e5a83511b7340ed5
VienLV <VienLV@example.com>
2019-05-21 14:18:23 +0700

    add bootstrap other

--- frontend/config/main.php
+++ frontend/config/main.php
@@ -14,6 +14,20 @@ return [
     'controllerNamespace' => 'frontend\controllers',
     'defaultRoute' => 'cms/home',
     'components' => [
+        'assetManager' => [
+            'bundles' => [
+//            'yii\web\JqueryAsset' => [
+//                'js'=>[]
+//            ],
+//            'yii\bootstrap\BootstrapPluginAsset' => [
+//                'js'=>[]
+//            ],
+                'yii\bootstrap\BootstrapAsset' => [
+                    'css' => ['/css/bootstrap.css'],
+                ],
+
+            ],
+        ],
         'authClientCollection' => [
             'class' => 'yii\authclient\Collection',
             'clients' => [
