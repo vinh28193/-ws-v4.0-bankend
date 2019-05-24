399b0de399306fedff07c3c760d79818604be0ac
Hoang Cong Phuc <phuchc@peacesoft.net>
2019-05-24 20:48:22 +0700

    Ga All pages check + Checkout Pages

--- frontend/modules/checkout/views/shipping/index.php
+++ frontend/modules/checkout/views/shipping/index.php
@@ -6,6 +6,43 @@
 /* @var frontend\modules\payment\models\ShippingForm $shippingForm */
 /* @var array $provinces */
 
+/** GA CHECKOUT **/
+$js_ga = <<<JS
+$(document).ready(function() {
+    var client = new ClientJS();
+            var _fingerprint = client.getFingerprint();
+            var data = {
+                fingerprint: _fingerprint,
+                path : window.location.pathname
+            };
+    // Ga Checkout
+  setTimeout(function () { 
+        ws.ajax('/frontend/u',{
+        type: 'POST',
+        dataType: 'json',
+        data: data,
+        loading: true,
+        success: function (result) {
+            //console.log(result);  console.log(result.success); 
+        }
+        });
+    }, 1000 * 1);
+  
+   setTimeout(function () { 
+        ws.ajax('/checkout/shipping/u',{
+        type: 'POST',
+        dataType: 'json',
+        data: data,
+        loading: true,
+        success: function (result) {
+            console.log(result);  console.log(result.success); 
+        }
+        });
+    }, 1000 * 1);
+});
+JS;
+$this->registerJs($js_ga, \yii\web\View::POS_END);
+
 $showStep = true;
 if($activeStep === 1){
 
