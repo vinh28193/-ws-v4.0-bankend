adce0f65116b60342895143ecb4074fd98230a2d
Hoang Cong Phuc <phuchc@peacesoft.net>
2019-05-25 20:22:42 +0700

    update post Curl FCM

--- api/modules/v1/controllers/FcmNotiController.php
+++ api/modules/v1/controllers/FcmNotiController.php
@@ -5,6 +5,8 @@ namespace api\modules\v1\controllers;
 use api\controllers\BaseApiController;
 use Yii;
 use yii\httpclient\Client;
+use linslin\yii2\curl;
+use yii\helpers\ArrayHelper;
 
 class FcmNotiController extends BaseApiController
 {
@@ -48,31 +50,44 @@ class FcmNotiController extends BaseApiController
 
     public function actionCreate()
     {
-        $_post = (array)$this->post;
-        if (isset($_post) !== null) {
-            $_to_token_fingerprint = "eFbHagcSYD8:APA91bH2bcY4fpKwMezwGJUOWJLyYb7vnmg5NbycLJftuRGxgFH4DYAOCHcjq2xUiMwCr4LRnUbKmYV1PukYkPnFNutJTLuJ_EK-uIzVPPYmo9tq8XP1xq2savPE0gjbjsWs-0snI6or"; // Mã UIDI thiết bị
-            $_title = "Message Title Weshop 2019";
-            $_body = "Message body Weshop 2019";
-            $_click_action = "https://weshop.com.vn";
-            $body = '{
-            "notification": {
-                "title": '. $_title . ',
-                 "body": '. $_body .',
-                 "click_action" : '. $_click_action .'
-             },
-             "to" : '. $_to_token_fingerprint .'}';
+        Yii::info("Send Start FCM");
+        $post = (array)$this->post;
+        if (isset($post) !== null) {
+            $_to_token_fingerprint = ArrayHelper::getValue($post,'token_fcm');
+            $_title = ArrayHelper::getValue($post,'title');
+            $_body = ArrayHelper::getValue($post,'messa_body');
+            $_click_action = ArrayHelper::getValue($post,'action');
 
-            $client = new Client();
-            $response = $client->createRequest()
-                ->setMethod('POST')
-                ->setFormat('json')
-                ->addHeaders(['Authorization'=> $this->key])
-                ->setUrl($this->fcm_google)
-                ->setData($body)
-                ->setOptions([
-                    'timeout' => 90, // set timeout to 5 seconds for the case server is not responding
-                ])
-                ->send();
+            $fcm_json = new \stdClass();
+            $json_text = new \stdClass();
+            $json_text->title = $_title;
+            $json_text->body = $_body;
+            $json_text->click_action = $_click_action;
+            $fcm_json->notification = $json_text;
+            $fcm_json->to = $_to_token_fingerprint;
+            $now_body_send = $fcm_json;
+
+            Yii::info([
+                'to_token_fingerprint' => $_to_token_fingerprint ,
+                'title ' => $_title ,
+                '_body' => $_body ,
+                'click_action' => $_click_action ,
+                //'body' => $body,
+                'now_body_send' => $now_body_send,
+                'fcm_google' => $this->fcm_google,
+                'key' => $this->key
+            ], __CLASS__);
+
+            $client = new curl\Curl();
+            $response = $client->setHeaders([
+                            'Content-Type' => 'application/json',
+                            'Content-Length' => strlen(json_encode($now_body_send)),
+                            'Authorization'=> $this->key
+                        ])
+                ->setRawPostData(json_encode($now_body_send))
+                ->post($this->fcm_google);
+
+            Yii::info("ket qua send : ".$response);
 
             return $this->response(true, 'Success', $response);
         } else {
