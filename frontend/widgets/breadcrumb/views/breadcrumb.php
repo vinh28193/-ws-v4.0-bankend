5fa93741a87b149118f02762aa120123ab4b15d0
Vinh (envy) <vinhvv@peacesoft.net>
2019-06-01 09:50:08 +0700

    i18n

--- frontend/widgets/breadcrumb/views/breadcrumb.php
+++ frontend/widgets/breadcrumb/views/breadcrumb.php
@@ -8,12 +8,14 @@
 
 <nav aria-label="breadcrumb">
     <ol class="breadcrumb">
-        <li class="breadcrumb-item"><a href="/">Trang chủ</a></li>
-        <li class="breadcrumb-item"><a href="/">Weshop Global</a></li>
+        <li class="breadcrumb-item"><a href="/"><?=Yii::t('frontend','Home');?></a></li>
+        <li class="breadcrumb-item"><a href="/"><?=Yii::t('frontend','Weshop Global');?></a></li>
         <li class="breadcrumb-item"><a href="/<?= strtolower($portal); ?>.html"> <?php if( strtolower($portal) == "ebay") { echo 'Shop Ebay';}  if( strtolower($portal) == "amazon") { echo 'Shop Amazon';}  if( strtolower($portal) == "amazon-jp") { echo 'Shop Amazon Japan';} if( strtolower($portal) == "amazon-uk") { echo 'Shop Amazon Japan';} ?>   </a></li>
         <?= $Ekey = Yii::$app->request->get('keyword','') ?>
         <?php if ($Ekey != ''){?>
-            <li class="breadcrumb-item active">Tìm kiếm từ khóa <?php  echo $Ekey; ?></li>
+            <li class="breadcrumb-item active"><?=Yii::t('frontend','Search keyword {keyword}',[
+                    'keyword' => $Ekey
+                ]);?> </li>
         <?php } ?>
     </ol>
 </nav>
