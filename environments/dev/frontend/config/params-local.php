ce94a2aafcd9ff37e0a2cea556ae5540a7486d68
VienLV <VienLV@example.com>
2019-06-07 10:09:58 +0700

    fix enviroments

--- environments/dev/frontend/config/params-local.php
+++ environments/dev/frontend/config/params-local.php
@@ -4,6 +4,6 @@ return [
     'supportEmail'=> 'csadmin@weshop.com.vn',
     'Url_User_Back_end'=>'http://weshop-v4.back-end-user.local.vn',
     'Url_FrontEnd'=>'http://weshop-v4.front-end-ws.local.vn',
-    'Url_wallet_api' => 'http://weshop-4.0.wallet.vn',
+//    'Url_wallet_api' => 'http://weshop-4.0.wallet.vn',
     'ENV' => true // True --> envaroment Develop , false : Prod
 ];
