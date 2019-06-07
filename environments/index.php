03564210c0d053c711fafd5e535286f97264dddd
Hoang Cong Phuc <phuchc@peacesoft.net>
2019-06-07 20:53:16 +0700

    ENV Oracle

--- environments/index.php
+++ environments/index.php
@@ -185,4 +185,31 @@ return [
             'wallet/config/main-local.php',
         ],
     ],
+
+    // starging
+    'Starging' => [
+        'path' => 'starging',
+        'setWritable' => [
+            'api/runtime',
+            'api/web/assets',
+            'backend/runtime',
+            'backend/web/assets',
+            'userbackend/runtime',
+            'userbackend/web/assets',
+            'frontend/runtime',
+            'frontend/web/assets',
+            'wallet/runtime',
+            'wallet/web/assets',
+        ],
+        'setExecutable' => [
+            'yii',
+        ],
+        'setCookieValidationKey' => [
+            'api/config/main-local.php',
+            'backend/config/main-local.php',
+            'frontend/config/main-local.php',
+            'userbackend/config/main-local.php',
+            'wallet/config/main-local.php',
+        ],
+    ],
 ];
