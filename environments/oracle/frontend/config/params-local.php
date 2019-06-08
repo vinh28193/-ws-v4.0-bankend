0152459ac50cb3a0db7fd1ae49e14de8adaa64e4
Hoang Cong Phuc <phuchc@peacesoft.net>
2019-06-08 10:22:59 +0700

    ENV Bxd

---
+++ environments/oracle/frontend/config/params-local.php
@@ -0,0 +1,9 @@
+<?php
+return [
+    'user.passwordResetTokenExpire' => 3600,
+    'supportEmail'=> 'csadmin@weshop.com.vn',
+    'Url_User_Back_end'=>'http://weshop-v4.back-end-user.local.vn',
+    'Url_FrontEnd'=>'http://weshop-v4.front-end-ws.local.vn',
+    'Url_wallet_api' => 'http://weshop-4.0.wallet.vn',
+    'ENV' => true // True --> envaroment Develop , false : Prod
+];
