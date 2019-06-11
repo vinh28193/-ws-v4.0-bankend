39ac5d8455f76a25d54ebd4e1c2f4fa7ecfc3c89
VienLV <VienLV@example.com>
2019-06-11 09:29:58 +0700

    home page

--- frontend/widgets/layout/views/slides.php
+++ frontend/widgets/layout/views/slides.php
@@ -5,16 +5,16 @@
     <!-- The slideshow -->
     <div class="carousel-inner">
         <div class="carousel-item active">
-            <img src="https://static-v3.weshop.com.vn/uploadImages/cloud/cms/19/05/27/2/w/r/imgpsh_fullsize_anim.jpg"
-                 alt="Los Angeles" width="1100" height="500">
+            <img src="http://weshop.upload.local/uploadImages/cloud/0/7/8/078253aa98bddbc685675df76622a6dd.jpg"
+                 alt="" width="1100" height="500">
         </div>
         <div class="carousel-item">
-            <img src="https://static-v3.weshop.com.vn/uploadImages/cloud/cms/19/05/15/u/f/d/Home.jpg" alt="Chicago"
+            <img src="http://weshop.upload.local/uploadImages/cloud/a/d/e/ade62b1a7e29d6df1e8baac906994b25.jpg" alt=""
                  width="1100" height="500">
         </div>
         <div class="carousel-item">
-            <img src="https://static-v3.weshop.com.vn/uploadImages/cloud/cms/19/05/27/2/w/r/imgpsh_fullsize_anim.jpg"
-                 alt="New York" width="1100" height="500">
+            <img src="http://weshop.upload.local/uploadImages/cloud/1/4/e/14e03761a378b5cf42b58d1d21531fa5.jpg"
+                 alt="" width="1100" height="500">
         </div>
     </div>
 
@@ -25,53 +25,4 @@
     <a class="carousel-control-next" href="#slides" data-slide="next">
         <span class="la la-angle-right btn-change-slides"></span>
     </a>
-</div>
-<div class="content-body">
-    <h3>Kênh mua sắm hàng Mỹ tin cậy</h3>
-    <div class="row m-auto">
-        <div class="col-md-3">
-            <div class="row">
-                <div class="col-md-2">
-                    <i class="la la-check-circle"></i>
-                </div>
-                <div class="col-md-8">
-                    <div><b>An tâm mua sắm</b></div>
-                    <div>Thủ tục đơn giản, thanh toán tiện lợi nhanh chóng.</div>
-                </div>
-            </div>
-        </div>
-        <div class="col-md-3">
-            <div class="row">
-                <div class="col-md-2">
-                    <i class="la la-check-circle"></i>
-                </div>
-                <div class="col-md-8">
-                    <div><b>Phí dịch vụ cạnh tranh</b></div>
-                    <div>Dịch vụ đa dạng giúp bạn linh động & tối ưu chi phí.</div>
-                </div>
-            </div>
-        </div>
-        <div class="col-md-3">
-            <div class="row">
-                <div class="col-md-2">
-                    <i class="la la-check-circle"></i>
-                </div>
-                <div class="col-md-8">
-                    <div><b>Giao hàng tận nhà</b></div>
-                    <div>Vận chuyển quốc tế giao hàng tới tận nhà.</div>
-                </div>
-            </div>
-        </div>
-        <div class="col-md-3">
-            <div class="row">
-                <div class="col-md-2">
-                    <i class="la la-check-circle"></i>
-                </div>
-                <div class="col-md-8">
-                    <div><b>Sản phẩm đa dạng</b></div>
-                    <div>Có sẵn hàng triệu sản phẩm, cập nhật khuyến mãi liên tục.</div>
-                </div>
-            </div>
-        </div>
-    </div>
 </div>
