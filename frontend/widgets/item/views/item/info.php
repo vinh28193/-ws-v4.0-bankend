22f377cb266178d5652c488abaef61079b6e6607
Hoang Cong Phuc <phuchc@peacesoft.net>
2019-06-19 16:11:30 +0700

    update fixed hiden code php

--- frontend/widgets/item/views/item/info.php
+++ frontend/widgets/item/views/item/info.php
@@ -84,9 +84,11 @@ $this->registerCss($css);
             </strong>
             <?php if ($item->start_price) { ?>
                 <b class="old-price"><?= $storeManager->showMoney($item->getLocalizeTotalStartPrice()) ?></b>
-                <!--<span class="save"> <?/*= Yii::t('frontend', 'Save off: {percent}', [
+                <!--<span class="save"> <?php
+                /* Yii::t('frontend', 'Save off: {percent}', [
                         'percent' => $storeManager->showMoney($item->getLocalizeTotalStartPrice() - $item->getLocalizeTotalPrice())
-                    ]); */?>
+                    ]); */
+                ?>
                 </span>-->
             <?php } // Start start_price ?>
         </div>
