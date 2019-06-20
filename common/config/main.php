c6c1a02c8f95fef7271664a80f4aabfcaf3e32c9
huyvd <huyvd@peacesoft.net>
2019-06-20 20:11:37 +0700

    fix-fee

--- common/config/main.php
+++ common/config/main.php
@@ -24,6 +24,7 @@ return [
             'database' => 0,
         ],
         */
+        /*
         'cache' => [
             'class' => 'yii\redis\Cache',
             'redis' => [
@@ -32,7 +33,7 @@ return [
                 'database' => 0,
             ]
         ],
-        /*
+        */
         'cache' => [
             'class' => 'yii\redis\Cache',
             'redis' => [
@@ -41,7 +42,6 @@ return [
                 'database' => 0,
             ]
         ],
-        */
         'api' => [
             'class' => 'common\components\Api',
         ],
