eed96386549406481fb982a308f0f91469610665
Vinh (envy) <vinhvv@peacesoft.net>
2019-06-21 16:07:25 +0700

    exrate

--- frontend/modules/checkout/views/billing/fail.php
+++ frontend/modules/checkout/views/billing/fail.php
@@ -6,10 +6,25 @@ use yii\helpers\Html;
 /* @var string $code */
 /* @var frontend\modules\payment\Payment $payment */
 
+
 ?>
 
-<div class="card card-checkout">
-    <div class="card-body">
+<style type="text/css">
+    .center-box {
+        text-align: center;
+    }
+</style>
+<div class="container">
+    <div class="card card-checkout">
+        <div class="card-body">
+            <div class="center-box">
+                <img src="/images/icon/payment_fail.png" alt="" title=""/>
 
+                <p><?= Yii::t('frontend', 'Your shopping cart is empty'); ?></p>
+                <a href="#" class="btn btn-continue"
+                   onclick="window.location.href = '/'"><?= Yii::t('frontend', 'Continue to buy'); ?></a>
+            </div>
+        </div>
     </div>
 </div>
+
