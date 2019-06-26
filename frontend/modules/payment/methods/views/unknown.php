1408e26bea6f9e14e4a8a32c14a3ce2bf313cd7a
Vinh (envy) <vinhvv@peacesoft.net>
2019-06-26 09:25:35 +0700

    unknown

--- frontend/modules/payment/methods/views/unknown.php
+++ frontend/modules/payment/methods/views/unknown.php
@@ -6,17 +6,18 @@ use yii\helpers\Html;
 /* @var integer $group */
 /* @var frontend\modules\payment\Payment $payment */
 /* @var array $methods */
+/* @var boolean $selected */
 ?>
 
 
 <div class="method-item">
-    <a class="btn method-select" data-toggle="collapse" data-target="#method_unknown" aria-expanded="false">
-        <i class="icon method_unknown"></i>
-        <div class="name">unknown payment method group <?=$group;?></div>
+    <a class="btn method-select" data-toggle="collapse" data-target="#method_unknown<?=$group?>" aria-expanded="false">
+        <i class="icon method_unknown<?=$group?>"></i>
+        <div class="name">Unknown payment method group <?=$group;?></div>
         <div class="desc">please re config</div>
     </a>
 
-    <div id="method_unknown" class="collapse" aria-labelledby="headingOne" data-parent="#payment-method">
+    <div id="method_unknown<?=$group?>" class="<?= $selected ? 'collapse show' : 'collapse' ?>" aria-labelledby="headingOne" data-parent="#payment-method">
         <div class="method-content">
             <p><b>NOT FOUND</b></p>
         </div>
