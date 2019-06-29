a51489a3d314387cd0b0f7ef786fa5f3adb6f090
Vinh (envy) <vinhvv@peacesoft.net>
2019-06-29 21:06:18 +0700

    debug

--- common/components/InternationalShippingCalculator.php
+++ common/components/InternationalShippingCalculator.php
@@ -72,6 +72,7 @@ class InternationalShippingCalculator extends BaseObject
         $data = $response->getData();
         $success = $response->getError() === false && $data->count() > 0;
         $message = $response->getErrorMessage();
+        \Yii::info($message);
         if (!$success && WeshopHelper::isEmpty($message) && isset($status->details) && is_string($status->details)) {
             $message = $status->details;
         }
