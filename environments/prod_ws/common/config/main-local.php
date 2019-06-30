769b92f4fe0346513ae63ce3b6c92fb0af786279
Hoang Cong Phuc <phuchc@peacesoft.net>
2019-06-30 17:53:16 +0700

    update ENV live VN  mongo live WS

--- environments/prod_ws/common/config/main-local.php
+++ environments/prod_ws/common/config/main-local.php
@@ -72,7 +72,7 @@ return [
         ],
         'mongodb' => [
             'class' => '\yii\mongodb\Connection',
-            'dsn' => 'mongodb://backup:QuKUBS1cQ2Q@WSBOX2019@mongodbWs:27017/admin',
+            'dsn' => 'mongodb://backup:QuKUBS1cQ2Q@mongodbWs:27017/admin',
         ],
 //        'db_oauth'=>[
 //            'class' => 'yii\db\Connection',
