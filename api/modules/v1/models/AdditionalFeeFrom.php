47b389e5f52222042b6f594162785bccfd6a50e1
Vinh (envy) <vinhvv@peacesoft.net>
2019-07-01 11:55:42 +0700

    call update order

--- api/modules/v1/models/AdditionalFeeFrom.php
+++ api/modules/v1/models/AdditionalFeeFrom.php
@@ -9,6 +9,8 @@ use common\components\GetUserIdentityTrait;
 use common\components\InternationalShippingCalculator;
 use common\components\UserCookies;
 use common\helpers\WeshopHelper;
+use common\models\Order;
+use common\models\Product;
 use common\models\User;
 use common\products\BaseProduct;
 use Yii;
@@ -19,41 +21,116 @@ class AdditionalFeeFrom extends Model implements AdditionalFeeInterface
 {
     use GetUserIdentityTrait;
 
-    public $target = 'products';
+    public $target_name;
     public $target_id;
     public $store_id;
     public $customer_id;
-    public $item_type = 'calculator';
-    public $shipping_weight = 0;
-    public $shipping_quantity = 1;
+    public $item_type;
+    public $shipping_weight;
+    public $shipping_quantity;
 
-    public $us_amount = 0;
+    public $us_amount;
 
-    public $us_tax = 0;
+    public $us_tax;
 
-    public $us_ship = 0;
+    public $us_ship;
 
+
+    public function attributes()
+    {
+        return ArrayHelper::merge(parent::attributes(), [
+            'target_name', 'target_id', 'store_id', 'customer_id', 'item_type', 'shipping_weight', 'shipping_quantity', 'us_amount', 'us_tax', 'us_ship',
+        ]);
+    }
+
+    public function rules()
+    {
+        return ArrayHelper::merge(parent::rules(), [
+            [['target_name', 'target_id', 'store_id',], 'required'],
+            [['target_id', 'store_id', 'customer_id', 'shipping_weight', 'shipping_quantity'], 'integer'],
+            [['target_id', 'store_id', 'customer_id', 'shipping_weight', 'shipping_quantity'], 'filter', 'filter' => function ($value) {
+                return (integer)$value;
+            }],
+            [['us_amount', 'us_tax', 'us_ship'], 'number'],
+            ['target', 'string'],
+            ['target', 'default', 'value' => 'product'],
+
+        ]);
+    }
+
+    public function load($data, $formName = null)
+    {
+        return parent::load($data, $formName);
+    }
+
+    /** @var \common\components\db\ActiveRecord */
+    private $_target;
+
+    /**
+     * @return \common\components\db\ActiveRecord
+     */
+    public function getTarget()
+    {
+        if (!$this->_target) {
+            $class = $this->target_name == 'product' ? Product::className() : Order::className();
+            if (($target = $class::findOne($this->target_id)) !== null) {
+                $this->_target = $target;
+            }
+        }
+        return $this->_target;
+
+    }
 
     private $_additionalFees;
 
     public function getAdditionalFees()
     {
         if ($this->_additionalFees === null) {
-            $this->_additionalFees = new AdditionalFeeCollection();
-            if ($this->store_id !== null) {
-                $this->_additionalFees->getStoreManager()->setStore($this->store_id);
+            $this->_additionalFees = new AdditionalFeeCollectionCustom();
+            $this->_additionalFees->storeId = $this->store_id;
+            $this->_additionalFees->userId = $this->customer_id;
+            $this->_additionalFees->removeAll();
+            if ($this->getTarget() !== null) {
+                $this->_additionalFees->loadFormActiveRecord($this->getTarget(), $this->target_name);
             }
-            if ($this->customer_id !== null) {
-                $this->setUser($this->customer_id);
+            $hasChange = false;
+            $taxRate = $this->ensureTaxRate();
+            $usAmount = $this->us_amount;
+            $this->shipping_quantity = ($this->shipping_quantity !== null && $this->shipping_quantity !== '' && (int)$this->shipping_quantity > 0) ? $this->shipping_quantity : 1;
+            if($usAmount && $usAmount !== '' ){
+                $usAmount *= (int)$this->shipping_quantity;
             }
-            $this->_additionalFees->withConditions($this, [
-                'product_price' => $this->us_amount,
-                'shipping_fee' => $this->us_ship,
-                'tax_fee' => $this->us_tax
-            ], false);
-            $this->_additionalFees->withCondition($this, 'purchase_fee', null);
+            if ($usAmount !== null && $usAmount !== '' && !WeshopHelper::compareValue($usAmount, $this->_additionalFees->getTotalAdditionalFees('product_price')[0])) {
+                $this->_additionalFees->remove('product_price');
+                $this->_additionalFees->withCondition($this, 'product_price', $usAmount);
+                $this->_additionalFees->remove('tax_fee');
+                $this->_additionalFees->withCondition($this, 'tax_fee', $taxRate);
+                $hasChange = true;
+            }
+
+            if ($this->us_ship !== null && $this->us_ship !== '' && !WeshopHelper::compareValue($this->us_ship, $this->_additionalFees->getTotalAdditionalFees('shipping_fee')[0])) {
+                $this->_additionalFees->remove('shipping_fee', $this->us_ship);
+                $this->_additionalFees->withCondition($this, 'shipping_fee', $this->us_ship);
+                $this->_additionalFees->remove('tax_fee');
+                $this->_additionalFees->withCondition($this, 'tax_fee', $taxRate);
+                $hasChange = true;
+            }
+            if ($hasChange || $totalOrigin = $this->getTotalOrigin() > 0) {
+                $this->_additionalFees->remove('purchase_fee', $this->us_ship);
+                $this->_additionalFees->withCondition($this, 'purchase_fee', null);
+            }
+
         }
         return $this->_additionalFees;
+    }
+
+    protected function ensureTaxRate()
+    {
+        $taxRate = 0;
+        if (($taxFee = $this->getAdditionalFees()->getTotalAdditionalFees('tax_fee')[0]) > 0) {
+            $taxRate = ($taxFee * 100) / $this->getAdditionalFees()->getTotalAdditionalFees(['product_price', 'shipping_fee'])[0];
+        }
+        return $taxRate;
     }
 
     /**
@@ -93,10 +170,10 @@ class AdditionalFeeFrom extends Model implements AdditionalFeeInterface
      */
     public function getUserLevel()
     {
-        if ($this->getUser()) {
-            return $this->getUser()->userLevel;
+        if ($this->customer_id === null || $this->customer_id === '' || ($user = User::findOne($this->customer_id)) === null) {
+            return User::LEVEL_NORMAL;
         }
-        return User::LEVEL_NORMAL;
+        return $user->getUserLevel();
     }
 
     /**
