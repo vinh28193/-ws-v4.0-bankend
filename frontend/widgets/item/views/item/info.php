7d11fc434a8f78a525eb198ae402f5c8ce55856e
VienLV <VienLV@example.com>
2019-07-03 19:18:16 +0700

    fix prime

--- frontend/widgets/item/views/item/info.php
+++ frontend/widgets/item/views/item/info.php
@@ -66,7 +66,12 @@ $userCookies->setUser();
         <span>
             <?php
             if(is_array($item->providers) && count($item->providers)){
-              echo Yii::t('frontend','of seller <a href="#" id="seller_name">{seller}</a>',['seller' => $current_provider ? $current_provider->name : '---']);
+                echo Yii::t('frontend','of seller <a href="#" id="seller_name">{seller}</a>',['seller' => $current_provider ? $current_provider->name : '---']);
+            }
+            if($item->type === BaseProduct::TYPE_EBAY && $current_provider && $current_provider->positive_feedback_percent){
+                echo "<a href='javascript:void(0)' class=\"text-black\">&nbsp;&nbsp;&nbsp;(".Yii::t('frontend','{positive}% positive',['positive' => $current_provider->positive_feedback_percent]).")</a>";
+            }elseif($item->is_prime){
+                echo "<img style='height: 14px;padding-left: 5px;' src='/images/logo/prime.png'>";
             }
             ?>
         </span>
