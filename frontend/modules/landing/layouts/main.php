bea2ffafe07346c60d51acbb05e0dae3b5e4d7b6
VienLV <VienLV@example.com>
2019-07-03 09:39:01 +0700

    fix landing

--- frontend/modules/landing/layouts/main.php
+++ frontend/modules/landing/layouts/main.php
@@ -22,7 +22,7 @@ use yii\helpers\Url;
                                         <!-- Brand and toggle get grouped for better mobile display -->
                                         <div class="navbar-header">
                                             <a class="navbar-brand" href="/">
-                                                <img src="https://weshop.com.vn/images/weshop-logo.png"
+                                                <img src="/images/logo/weshop-02.png"
                                                      alt="Weshop Logo" title="Weshop Logo">
                                             </a>
                                         </div>
@@ -221,188 +221,6 @@ use yii\helpers\Url;
                     </div>
                     <div>
                         <ul class="nav navbar-nav navbar-right">
-                            <li class="dropdown buy-for-me">
-                                <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button"
-                                   aria-haspopup="true" aria-expanded="false">Mua hộ <i
-                                            class="fa fa-angle-down"></i></a>
-                                <div class="dropdown-menu buyforme-content animated ws-animated fadeInUpShort">
-                                    <span class="arrow"></span>
-                                    <div class="col-70 left">
-                                        <div class="step">
-                                            <ul>
-                                                <li class="icon">
-                                                    <div class="img"><span class="shop-icon step-1"></span></div>
-                                                    <div class="text">
-                                                        <div class="text-step">Bước 1</div>
-                                                        <p>Chọn hàng hoặc gửi link muốn mua</p>
-                                                    </div>
-                                                </li>
-                                                <li class="next" id="next1">
-                                                    <i class="fa fa-circle text-active"></i>
-                                                    <i class="fa fa-circle"></i>
-                                                    <i class="fa fa-circle"></i>
-                                                </li>
-                                                <li class="icon">
-                                                    <div class="img"><span class="shop-icon step-2"></span></div>
-                                                    <div class="text">
-                                                        <div class="text-step">Bước 2</div>
-                                                        <p>Thanh toán đơn hàng</p>
-                                                    </div>
-                                                </li>
-                                                <li class="next" id="next2">
-                                                    <i class="fa fa-circle"></i>
-                                                    <i class="fa fa-circle text-active"></i>
-                                                    <i class="fa fa-circle"></i>
-                                                </li>
-                                                <li class="icon">
-                                                    <div class="img"><span class="shop-icon step-3"></span></div>
-                                                    <div class="text">
-                                                        <div class="text-step">Bước 3</div>
-                                                        <p>Weshop hỗ trợ mua hàng, kiểm tra hàng</p>
-                                                    </div>
-                                                </li>
-                                                <li class="next" id="next3">
-                                                    <i class="fa fa-circle"></i>
-                                                    <i class="fa fa-circle"></i>
-                                                    <i class="fa fa-circle text-active"></i>
-                                                </li>
-                                                <li class="icon">
-                                                    <div class="img"><span class="shop-icon step-4"></span></div>
-                                                    <div class="text">
-                                                        <div class="text-step">Bước 4</div>
-                                                        <p>Weshop hỗ trợ chuyển hàng về tận tay bạn</p>
-                                                    </div>
-                                                </li>
-                                            </ul>
-                                        </div>
-                                    </div>
-                                    <div class="col-30 right">
-                                        <ul>
-                                            <li>
-                                                <a href="/amazon.html">
-                                                    <span class="title">Shop Amazon</span>
-                                                    <span class="desc">Các mặt hàng có chất lượng</span>
-                                                </a>
-                                            </li>
-                                            <li>
-                                                <a href="/ebay.html">
-                                                    <span class="title">Shop eBay</span>
-                                                    <span class="desc">Tìm sản phẩm độc đáo và siêu rẻ</span>
-                                                </a>
-                                            </li>
-                                            <li>
-                                                <a href="/brands.html">
-                                                    <span class="title">Mua sắm tại các cửa hàng hàng đầu tại Mĩ</span>
-                                                    <span class="desc">Các cửa hàng mua sắm mới</span>
-                                                </a>
-                                            </li>
-                                            <li>
-                                                <a href="/about-us.html">
-                                                    <span class="title">Về Weshop</span>
-                                                    <span class="desc">12 lợi ích khi mua sắm tại Weshop</span>
-                                                </a>
-                                            </li>
-                                            <li>
-                                                <a href="/paste-link.html">
-                                                    <span class="title">Mua sắm trên toàn thế giới</span>
-                                                    <span class="desc">Cho cuộc sống phong cách</span>
-                                                </a>
-                                            </li>
-                                        </ul>
-                                    </div>
-                                </div>
-                            </li>
-                            <li class="dropdown ship-for-me">
-                                <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button"
-                                   aria-haspopup="true" aria-expanded="false">Ship hộ <i
-                                            class="fa fa-angle-down"></i></a>
-                                <div class="dropdown-menu shipforme-content animated ws-animated fadeInUpShort">
-                                    <span class="arrow"></span>
-                                    <div class="col-70 left">
-                                        <div class="step">
-                                            <ul>
-                                                <li class="icon">
-                                                    <div class="img"><span class="ship-icon step-1"></span></div>
-                                                    <div class="text">
-                                                        <div class="text-step">Bước 1</div>
-                                                        <p>Đăng ký tài khoản Weshop Có ngay địa chỉ riêng tại Mỹ! </p>
-                                                    </div>
-                                                </li>
-                                                <li class="next" id="next1">
-                                                    <i class="fa fa-circle text-active"></i>
-                                                    <i class="fa fa-circle"></i>
-                                                    <i class="fa fa-circle"></i>
-                                                </li>
-                                                <li class="icon">
-                                                    <div class="img"><span class="ship-icon step-2"></span></div>
-                                                    <div class="text">
-                                                        <div class="text-step">Bước 2</div>
-                                                        <p>Tự mua tại bất kỳ website nào và chuyển hàng tới địa chỉ Mỹ
-                                                            của bạn.</p>
-                                                    </div>
-                                                </li>
-                                                <li class="next" id="next2">
-                                                    <i class="fa fa-circle"></i>
-                                                    <i class="fa fa-circle text-active"></i>
-                                                    <i class="fa fa-circle"></i>
-                                                </li>
-                                                <li class="icon">
-                                                    <div class="img"><span class="ship-icon step-3"></span></div>
-                                                    <div class="text">
-                                                        <div class="text-step">Bước 3</div>
-                                                        <p>Weshop hỗ trợ nhận hàng, kiểm hàng và chuyển quốc tế</p>
-                                                    </div>
-                                                </li>
-                                                <li class="next" id="next3">
-                                                    <i class="fa fa-circle"></i>
-                                                    <i class="fa fa-circle"></i>
-                                                    <i class="fa fa-circle text-active"></i>
-                                                </li>
-                                                <li class="icon">
-                                                    <div class="img"><span class="ship-icon step-4"></span></div>
-                                                    <div class="text">
-                                                        <div class="text-step">Bước 4</div>
-                                                        <p>Weshop hỗ trợ chuyển hàng tới tận tay bạn và thu tiền dịch
-                                                            vụ</p>
-                                                    </div>
-                                                </li>
-                                            </ul>
-                                        </div>
-                                    </div>
-                                    <div class="col-30 right">
-                                        <ul>
-                                            <li class="active"><a
-                                                        href="/faq/detail/lam-the-nao-de-toi-co-duoc-dia-chi-kho-hang-weshop-786.html"><i
-                                                            class="fa fa-angle-double-right"></i> Cung cấp địa chỉ của
-                                                    bạn
-                                                </a></li>
-                                            <li><a href="/helps/detail/mien-phi-kiem-dem-hang-co-ban-775.html"><i
-                                                            class="fa fa-angle-double-right"></i> Kiểm tra </a></li>
-                                            <li>
-                                                <a href="/helps/detail/cac-dich-vu-khac-chup-hinh-kiem-tra-chi-tiet-hang-yeu-cau-dac-biet-778.html"><i
-                                                            class="fa fa-angle-double-right"></i> Dịch vụ chụp ảnh </a>
-                                            </li>
-                                            <li><a href="/helps/detail/mien-phi-gom-hang-772.html"><i
-                                                            class="fa fa-angle-double-right"></i> Hợp nhất lô hàng </a>
-                                            </li>
-                                            <li><a href="/helps/detail/mien-phi-goi-lai-hang-773.html"><i
-                                                            class="fa fa-angle-double-right"></i> Dịch vụ đóng gói lại
-                                                </a>
-                                            </li>
-                                            <!--<li><a href="#"><i class="fa fa-angle-double-right"></i> Dịch vụ Đến nhận hàng</a></li>-->
-                                            <li><a href="/helps/detail/bao-hiem-hang-hoa-776.html"><i
-                                                            class="fa fa-angle-double-right"></i> Bảo hiểm vận chuyển
-                                                </a>
-                                            </li>
-                                            <!--<li><a href="#"><i class="fa fa-angle-double-right"></i> Dịch vụ kê khai Hải quan</a></li>-->
-                                            <li><a href="/helps/detail/mien-phi-luu-kho-toi-60-ngay-774.html"><i
-                                                            class="fa fa-angle-double-right"></i> Miễn phí 60 ngày lưu
-                                                    trữ
-                                                </a></li>
-                                        </ul>
-                                    </div>
-                                </div>
-                            </li>
                             <li class="dropdown order-tracking">
                                 <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button"
                                    aria-haspopup="true" aria-expanded="false">Kiểm tra hành trình đơn hàng <i
@@ -428,9 +246,7 @@ use yii\helpers\Url;
                             <li class="dropdown user-in ">
                                 <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button"
                                    aria-haspopup="true" aria-expanded="false">
-                                    <i class="user-avatar">
-                                        <img src="https://weshop.com.vn/account_images/logo/no-photo.png" alt=""
-                                             title="">
+                                    <i class="icon-nav icon-user">
                                     </i>
                                 </a>
                                 <div class="dropdown-menu animated ws-animated fadeInUpShort">
@@ -575,7 +391,7 @@ use yii\helpers\Url;
             <div class="container">
                 <div class="row">
                     <div class="col-md-3 header-logo">
-                        <a href="/"><img src="https://weshop.com.vn/images/logo/weshop/weshop-logo-vn.png" alt=""></a>
+                        <a href="/"><img src="/images/logo/weshop-02.png" alt=""></a>
                     </div>
                     <div class="col-md-6 header-search">
                         <div class="input-group">
