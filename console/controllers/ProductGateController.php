d06489d17f71d9e582b36ee41c14cca4e3783ec2
Vinh (envy) <vinhvv@peacesoft.net>
2019-07-10 09:21:07 +0700

    fix job

--- console/controllers/ProductGateController.php
+++ console/controllers/ProductGateController.php
@@ -28,7 +28,7 @@ class ProductGateController extends Controller
         $query = WsProduct::find()->where(['status' => 1]);
 
         $totalFetch = (clone $query)->count();
-        $limit = 1000;
+        $limit = 100;
         $totalPage = ceil($totalFetch / $limit);
         $totalCount = 0;
         for ($page = 1; $page <= $totalPage; $page++) {
@@ -38,6 +38,7 @@ class ProductGateController extends Controller
             $this->stdout("    > loading page $page/$totalPage, limit $limit, offset $offset ...\n", Console::FG_GREEN);
             $count = 0;
             foreach ($records as $record) {
+                $count++;
                 /** @var  $record WsProduct */
                 $log = new ProductSyncLog();
                 $rStart = microtime(true);
@@ -59,7 +60,7 @@ class ProductGateController extends Controller
                         $log->results = $form->getErrors();
                         $log->action = 'delete';
                         $log->save(false);
-                        $this->stdout("    > not found item {$form->type} {$form->id} {$form->sku} ", Console::FG_RED);
+                        $this->stdout("    > not found item {$form->type} {$form->id} {$form->sku} \n", Console::FG_RED);
                         $record->updateAttributes(['status' => 0]);
                         continue;
                     }
@@ -68,6 +69,26 @@ class ProductGateController extends Controller
                     $update['start_price'] = $product->start_price;
                     $update['calculated_sell_price'] = WeshopHelper::roundNumber($product->getLocalizeTotalPrice() / $product->getExchangeRate(), 2);
                     $update['calculated_start_price'] = WeshopHelper::roundNumber($product->getLocalizeTotalStartPrice() / $product->getExchangeRate(), 2);
+                    if ($product->getSellPrice() === 0 || count($product->providers) === 0) {
+                        $log->results = [
+                            'sell_price' => $product->getSellPrice(),
+                            'providers' => $product->providers
+                        ];
+                        $log->action = 'delete';
+                        $log->save(false);
+                        $record->updateAttributes(['status' => 0]);
+                        continue;
+                    }
+                    if ($record->provider !== null) {
+                        foreach ($product->providers as $provider) {
+                            if ($provider->name === $record->provider) {
+                                $product->updateBySeller($provider->prov_id);
+                                break;
+                            }
+                        }
+                    }
+                    $product->description = null;
+                    $product->technical_specific = null;
                     $log->results = [
                         'oldValue' => [
                             'weight' => $record->weight,
@@ -76,25 +97,27 @@ class ProductGateController extends Controller
                             'calculated_sell_price' => $record->calculated_sell_price,
                             'calculated_start_price' => $record->calculated_start_price,
                         ],
-                        'updateValue' => $update
+                        'updateValue' => $update,
+                        'product' => $product,
+                        'productFees' => $product->getAdditionalFees()->toArray()
                     ];
                     $log->action = 'complete';
                     $log->save(false);
                     $record->updateAttributes($update);
                     $rTime = sprintf('%.3f', microtime(true) - $rStart);
                     $this->stdout("    > committed record $count in page $page  (time $rTime s) \n", Console::FG_GREEN);
-                    sleep(20);
                 } catch (\Exception $exception) {
-                    $this->stdout("    > {$exception->getMessage()}", Console::FG_RED);
+                    $this->stdout("    > {$exception->getMessage()} \n", Console::FG_RED);
                     $log->results = $exception->getMessage();
                     $log->action = 'exception';
                     $log->save(false);
                     $record->updateAttributes(['status' => 0]);
                 }
-                $count++;
             }
             $pTime = sprintf('%.3f', microtime(true) - $pStart);
-            $this->stdout("    > committed $count records in page $page  (time $pTime s) \n", Console::FG_GREEN);
+            $this->stdout("    > committed $count records in page $page/$totalPage  (time $pTime s) \n", Console::FG_GREEN);
+            sleep(20);
+            $this->stdout("    > action will be continue after sleep.. \n", Console::FG_GREEN);
         }
         $time = sprintf('%.3f', microtime(true) - $start);
         $this->stdout("    > action ended execute $totalCount/$totalFetch records (time $time s) \n", Console::FG_GREEN);
