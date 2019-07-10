2e06b7f4827396a3e76125fdacf2cf9ac9685e48
Vinh (envy) <vinhvv@peacesoft.net>
2019-07-10 08:57:45 +0700

    fix job sync

--- console/controllers/ProductGateController.php
+++ console/controllers/ProductGateController.php
@@ -7,6 +7,7 @@ namespace console\controllers;
 use common\additional\AdditionalFeeCalculator;
 use common\helpers\WeshopHelper;
 use common\models\cms\WsProduct;
+use common\modelsMongo\ProductSyncLog;
 use common\products\forms\ProductDetailFrom;
 use yii\console\Controller;
 use Yii;
@@ -38,7 +39,15 @@ class ProductGateController extends Controller
             $count = 0;
             foreach ($records as $record) {
                 /** @var  $record WsProduct */
+                $log = new ProductSyncLog();
                 $rStart = microtime(true);
+                $itemType = WeshopHelper::isSubText($record->item_url, 'ebay/item') ? 'ebay' : 'amazon';
+                $log->time = $now;
+                $log->item_type = $itemType;
+                $log->item_id = $record->item_id;
+                $log->item_sku = $record->item_sku;
+                $log->row_id = $record->id;
+
                 $this->stdout("    > syncing record $count/$limit \n", Console::FG_GREEN);
                 $this->stdout("    > record {$record->id} \n", Console::FG_GREEN);
                 try {
@@ -47,6 +56,9 @@ class ProductGateController extends Controller
                     $form->id = $record->item_id;
                     $form->sku = $record->item_sku;
                     if (($product = $form->detail()) === false) {
+                        $log->results = $form->getErrors();
+                        $log->action = 'delete';
+                        $log->save(false);
                         $this->stdout("    > not found item {$form->type} {$form->id} {$form->sku} ", Console::FG_RED);
                         $record->updateAttributes(['status' => 0]);
                         continue;
@@ -56,12 +68,27 @@ class ProductGateController extends Controller
                     $update['start_price'] = $product->start_price;
                     $update['calculated_sell_price'] = WeshopHelper::roundNumber($product->getLocalizeTotalPrice() / $product->getExchangeRate(), 2);
                     $update['calculated_start_price'] = WeshopHelper::roundNumber($product->getLocalizeTotalStartPrice() / $product->getExchangeRate(), 2);
+                    $log->results = [
+                        'oldValue' => [
+                            'weight' => $record->weight,
+                            'sell_price' => $record->sell_price,
+                            'start_price' => $record->start_price,
+                            'calculated_sell_price' => $record->calculated_sell_price,
+                            'calculated_start_price' => $record->calculated_start_price,
+                        ],
+                        'updateValue' => $update
+                    ];
+                    $log->action = 'complete';
+                    $log->save(false);
                     $record->updateAttributes($update);
                     $rTime = sprintf('%.3f', microtime(true) - $rStart);
                     $this->stdout("    > committed record $count in page $page  (time $rTime s) \n", Console::FG_GREEN);
-                    sleep(5 * 60);
+                    sleep(20);
                 } catch (\Exception $exception) {
                     $this->stdout("    > {$exception->getMessage()}", Console::FG_RED);
+                    $log->results = $exception->getMessage();
+                    $log->action = 'exception';
+                    $log->save(false);
                     $record->updateAttributes(['status' => 0]);
                 }
                 $count++;
