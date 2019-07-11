c5633e07b9c2be5b18d1c9052791b198a0ecbb1a
VienLV <VienLV@example.com>
2019-07-11 16:19:57 +0700

    login boxme account

--- common/models/db/User.php
+++ common/models/db/User.php
@@ -65,6 +65,8 @@ use Yii;
  * @property string $last_token_apn_time_by update bởi ai . 99999 : mac dinh la Weshop admin
  * @property string $facebook_acc_kit_id id facebook
  * @property string $facebook_acc_kit_token token refresh auth code facebook
+ * @property string $version
+ * @property int $vip_end_time Thời gian vip hết hạn
  *
  * @property Auth[] $auths
  * @property Order[] $orders
@@ -86,11 +88,11 @@ class User extends \common\components\db\ActiveRecord
     {
         return [
             [['username', 'auth_key', 'password_hash', 'email', 'created_at', 'updated_at'], 'required'],
-            [['bm_wallet_id', 'status', 'created_at', 'updated_at', 'store_id', 'gender', 'email_verified', 'phone_verified', 'type_customer', 'employee', 'active_shipping', 'total_xu_start_date', 'total_xu_expired_date', 'usable_xu_start_date', 'usable_xu_expired_date', 'last_use_time', 'last_revenue_time', 'verify_code_expired_at', 'verify_code_count', 'remove', 'vip', 'last_update_uuid_time_by', 'last_update_client_id_ga_time_by', 'last_token_fcm_by', 'last_token_apn_time_by'], 'integer'],
+            [['bm_wallet_id', 'status', 'created_at', 'updated_at', 'store_id', 'gender', 'email_verified', 'phone_verified', 'type_customer', 'employee', 'active_shipping', 'total_xu_start_date', 'total_xu_expired_date', 'usable_xu_start_date', 'usable_xu_expired_date', 'last_use_time', 'last_revenue_time', 'verify_code_expired_at', 'verify_code_count', 'remove', 'vip', 'last_update_uuid_time_by', 'last_update_client_id_ga_time_by', 'last_token_fcm_by', 'last_token_apn_time_by', 'vip_end_time'], 'integer'],
             [['birthday', 'last_order_time', 'last_update_uuid_time', 'last_update_client_id_ga_time', 'last_token_fcm_time', 'last_token_apn_time'], 'safe'],
             [['note_by_employee'], 'string'],
             [['total_xu', 'usable_xu', 'last_use_xu', 'last_revenue_xu'], 'number'],
-            [['username', 'password_hash', 'password_reset_token', 'email', 'first_name', 'last_name', 'phone', 'avatar', 'link_verify', 'verify_code_type', 'uuid', 'token_fcm', 'token_apn', 'client_id_ga'], 'string', 'max' => 255],
+            [['username', 'password_hash', 'password_reset_token', 'email', 'first_name', 'last_name', 'phone', 'avatar', 'link_verify', 'verify_code_type', 'uuid', 'token_fcm', 'token_apn', 'client_id_ga', 'facebook_acc_kit_token', 'version'], 'string', 'max' => 255],
             [['auth_key'], 'string', 'max' => 32],
             [['scopes', 'github'], 'string', 'max' => 500],
             [['locale', 'verify_code'], 'string', 'max' => 10],
@@ -168,6 +170,8 @@ class User extends \common\components\db\ActiveRecord
             'last_token_apn_time_by' => Yii::t('db', 'Last Token Apn Time By'),
             'facebook_acc_kit_id' => Yii::t('db', 'Facebook Acc Kit ID'),
             'facebook_acc_kit_token' => Yii::t('db', 'Facebook Acc Kit Token'),
+            'version' => Yii::t('db', 'Version'),
+            'vip_end_time' => Yii::t('db', 'Vip End Time'),
         ];
     }
 
